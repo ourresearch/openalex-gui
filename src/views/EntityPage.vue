2d0456f431a02ccd930871db173516d3a6d6de73
Jason Priem <jp@jasonpriem.org>
2021-12-12 21:35:14 -0800

    pretty-print json.

--- src/views/EntityPage.vue
+++ src/views/EntityPage.vue
@@ -16,14 +16,31 @@
         <v-icon right>mdi-open-in-new</v-icon>
 
       </v-btn>
+
+      <v-card class="pa-5" color="#fafafa">
+      <div class="text-h4">
+        JSON data
+      </div>
+      <div class="body-2 mb-9">(click brackets to collapse/expand objects)</div>
+
+      <vue-json-pretty :data="apiResp" />
+
+      </v-card>
+
+
 <!--      <works-entity v-if="entityType==='works'" :data="apiResp"/>-->
-      <works-entity :data="apiResp"/>
+<!--      <works-entity :data="apiResp"/>-->
 
     </div>
   </v-container>
 </template>
 
 <script>
+
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+
+
 
 import {api} from "../api";
 import WorksEntity from "../components/WorksEntity";
@@ -37,6 +54,7 @@ export default {
   },
   components: {
     WorksEntity,
+    VueJsonPretty,
   },
   props: {},
   data() {
