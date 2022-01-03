ec50e2beb741a66870135413fe7c2e591a71c3e5
Jason Priem <jp@jasonpriem.org>
2022-01-02 23:41:28 -0800

    add oa to works.

--- src/components/EntityWork.vue
+++ src/components/EntityWork.vue
@@ -1,8 +1,9 @@
 <template>
   <v-container>
     <div class="">
-      📄 <strong>Work</strong><template v-if="data.type"> ({{ data.type }})</template>
-      <a :href="data.id" class="black--text d-block body-2">{{data.id}}</a>
+      📄 <strong>Work</strong>
+      <template v-if="data.type"> ({{ data.type }})</template>
+      <a :href="data.id" class="black--text d-block body-2">{{ data.id }}</a>
     </div>
 
     <h1 class="text-h3 my-6">{{ data.title }}</h1>
@@ -13,9 +14,9 @@
         {{ data.host_venue.display_name }}.
       </a>
     </div>
-      <a :href="data.ids.doi" class="grey--text body-2 d-block">
+    <a :href="data.ids.doi" class="grey--text body-2 d-block">
       {{ data.ids.doi }}
-      </a>
+    </a>
 
     <div class="mt-2">
       <link-concept
@@ -24,6 +25,18 @@
           :data="concept"
       />
     </div>
+    <div class="mt-2">
+      <div v-if="!data.open_access.is_oa">
+        <v-icon small>mdi-lock-outline</v-icon>
+        Closed access
+      </div>
+      <div v-if="data.open_access.is_oa" class="text-capitalize">
+        <v-icon small>mdi-lock-open-outline</v-icon>
+        {{ data.open_access.oa_status}}
+        Open Access
+      </div>
+
+    </div>
 
     <div class="mt-8">
       <div
@@ -31,13 +44,13 @@
           :key="authorship.author.id"
           class="authorship"
       >
-        <link-author :data="authorship.author" class="body-1" />
+        <link-author :data="authorship.author" class=""/>
         <span class="ml-2 institutions-list body-2">
           (<template v-for="(institution, i) in authorship.institutions">
               <template v-if="i > 0">, </template><link-institution
-                  :key="institution.id"
-                  :data="institution"
-              />
+            :key="institution.id"
+            :data="institution"
+        />
             </template>)
 
         </span>
@@ -54,7 +67,7 @@
         View work
       </v-btn>
       <v-btn v-if="data.open_access.oa_status==='green'">
-        <v-icon left >mdi-lock-open-outline</v-icon>
+        <v-icon left>mdi-lock-open-outline</v-icon>
         View free copy
       </v-btn>
     </div>
@@ -88,10 +101,10 @@ export default {
   },
   methods: {},
   computed: {
-    workIsFreeAtPublisher(){
+    workIsFreeAtPublisher() {
       return this.data.open_access.is_oa && this.data.open_access.oa_status !== "green"
     },
-    apiUrl(){
+    apiUrl() {
       return this.data.id + ".json"
       const shortId = this.data.id.replace("https://openalex.org/", "")
       return `https://api.openalex.org/work/${shortId}`
