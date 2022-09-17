1be74385c2be75207370de9aeeb3b84cf63ba122
Jason Priem <jp@jasonpriem.org>
2022-09-17 12:49:01 -0700

    entity author done.

--- src/components/EntityAuthor.vue
+++ src/components/EntityAuthor.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <table>
+      <tr v-if="data.last_known_institution">
+        <td class="table-row-label">
+          <entity-icon
+              type="institutions"
+              expand
+              singular
+          />
+        </td>
+        <td>
+          <link-to-entity
+              :entity="data.last_known_institution"
+          />
+        </td>
+      </tr>
+
       <tr>
         <td class="table-row-label">
           <entity-icon
@@ -13,54 +28,27 @@
         </td>
       </tr>
 
-      <tr v-if="data.last_known_institution">
+      <tr>
         <td class="table-row-label">
           <entity-icon
-              type="institutions"
+              type="works"
               expand
           />
         </td>
         <td>
-          <router-link
-              v-if="data.last_known_institution.id"
-              :to="data.last_known_institution.id | idLink"
-              class="text-decoration-none"
-          >
-            <span class="font-weight-bold">{{ data.cited_by_count.toLocaleString() }}</span> works
-          </router-link>
+          <link-to-search
+            :count="data.works_count"
+            filter-key="author.id"
+            :filter-value="data.id"
+            entity-type="works"
+          />
         </td>
       </tr>
-
 
     </table>
 
 
-    <div v-if="data.last_known_institution && data.last_known_institution.id">
-      <link-institution :data="data.last_known_institution"/>
-    </div>
-
-    <div class="mt-2">
-      <link-concept
-          v-for="concept in data.x_concepts"
-          :key="concept.id"
-          :data="concept"
-      />
-    </div>
-
-
-    <div class="mt-8">
-      <view-in-api-button :id="data.id" />
-    </div>
-    <v-divider class="mt-12 pt-12" />
-
-    <div class="text-h4">Identifiers</div>
-    <id-list :data="data.ids" />
-
-    <div class="text-h4 mt-12">
-      {{data.works_count.toLocaleString()}} created works
-    </div>
-    Click to view in API: <a :href="data.works_api_url" target="_blank">{{data.works_api_url}}</a>
-
+<!--    <id-list :data="data.ids"/>-->
 
 
   </div>
@@ -76,6 +64,8 @@ import IdList from "./IdList";
 import ViewInApiButton from "./ViewInApiButton";
 import EntityIcon from "./EntityIcon";
 import ConceptsList from "./ConceptsList";
+import LinkToEntity from "./LinkToEntity";
+import LinkToSearch from "./LinkToSearch";
 
 export default {
   name: "EntityAuthor",
@@ -86,6 +76,8 @@ export default {
     ViewInApiButton,
     EntityIcon,
     ConceptsList,
+    LinkToEntity,
+    LinkToSearch,
   },
   props: {
     data: Object,
@@ -96,9 +88,7 @@ export default {
     }
   },
   methods: {},
-  computed: {
-
-  },
+  computed: {},
   created() {
   },
   mounted() {
@@ -108,7 +98,13 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
+table {
+  td.table-row-label {
+    white-space: nowrap;
+    vertical-align: top;
+  }
+}
 
 </style>
