a2805ea028910717e59946c8fa0c8a3d23dd7c34
Jason Priem <jp@jasonpriem.org>
2023-04-16 21:45:58 -0700

    save point.

--- src/views/Serp.vue
+++ src/views/Serp.vue
@@ -14,6 +14,9 @@
 
     <v-container>
       <v-row>
+<!--        <v-col cols="2">-->
+<!--          -->
+<!--        </v-col>-->
         <v-col cols="12" sm="8">
           <v-card outlined class="mb-12">
             <serp-filters-list :single-work="singleWorkIdToShow" />
