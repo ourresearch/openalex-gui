c3613607f170ce10817ada5ce04b141a32180857
Jason Priem <jason@ourresearch.org>
2023-04-21 09:59:44 -0700

    you can search filters column.

--- src/components/SerpFacetsColumn.vue
+++ src/components/SerpFacetsColumn.vue
@@ -1,29 +1,54 @@
 <template>
   <v-card flat>
-    <v-toolbar flat>
-      <v-toolbar-title>
-        Filters
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click="openFacetsDialog">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+    <v-toolbar flat dense>
+      <v-text-field
+            flat
+            hide-details
+            full-width
+            clearable
+            prepend-inner-icon="mdi-magnify"
+            dense
 
+            v-model="searchString"
+            placeholder="Filters"
+        />
+
+<!--      <v-toolbar-title>-->
+<!--        Filters-->
+<!--      </v-toolbar-title>-->
+<!--      <v-spacer></v-spacer>-->
+      <!--      <v-btn icon @click="openFacetsDialog">-->
+      <!--        <v-icon>mdi-magnify</v-icon>-->
+      <!--      </v-btn>-->
+
+<!--      <template v-slot:extension>-->
+<!--        <v-text-field-->
+<!--            flat-->
+<!--            hide-details-->
+<!--            full-width-->
+<!--            clearable-->
+<!--            prepend-inner-icon="mdi-magnify"-->
+<!--            dense-->
+
+<!--            v-model="searchString"-->
+<!--            placeholder=""-->
+<!--        />-->
+<!--      </template>-->
     </v-toolbar>
     <v-list dense nav>
       <template
-              v-for="facetCategory in facetsByCategory()"
+          v-for="facetCategory in facetsByCategory(this.searchString)"
       >
         <v-subheader :key="'subheader'+facetCategory.name">
           {{ facetCategory.name }}
         </v-subheader>
-        <v-divider :key="'subheader-divider'+facetCategory.name" />
+        <v-divider :key="'subheader-divider'+facetCategory.name"/>
         <facet-simple
-                            v-for="facet in facetCategory.facets"
-                            :key="facet.entityType + facet.key"
-                            :facet-key="facet.key"
-                            :facet-entity-type="entityType"
-                    />
+            v-for="facet in facetCategory.facets"
+            :key="facet.entityType + facet.key"
+            :facet-key="facet.key"
+            :facet-entity-type="entityType"
+        />
 
       </template>
 
@@ -38,52 +63,52 @@ import {facetsByCategory} from "../facetConfigs";
 import FacetSimple from "./Facet/FacetSimple.vue";
 
 export default {
-    name: "SerpFacetsColumn",
-    components: {
-        FacetSimple,
-    },
-    props: {},
-    data() {
-        return {
-            foo: 42,
-        }
-    },
-    computed: {
-        ...mapGetters([
-            "resultsFilters",
-            "entityType",
-            "facetsByCategory",
-        ]),
-        isOpen: {
-            get() {
-                if (!this.$vuetify.breakpoint.mobile) return true
-                return this.$store.state.showFiltersDrawer
-            },
-            set(val) {
-                if (!this.$vuetify.breakpoint.mobile) return // you can't falsify isOpen on desktop
-                this.$store.state.showFiltersDrawer = val
-            },
-        },
-    },
-
-
-    methods: {
-        ...mapMutations([
-            "snackbar",
-            "openFacetsDialog",
-        ]),
-        ...mapActions([]),
-
-
-    },
-    created() {
-    },
-    mounted() {
-    },
-    watch: {
-        isOpen(to, from) {
-        }
+  name: "SerpFacetsColumn",
+  components: {
+    FacetSimple,
+  },
+  props: {},
+  data() {
+    return {
+      searchString: "",
     }
+  },
+  computed: {
+    ...mapGetters([
+      "resultsFilters",
+      "entityType",
+      "facetsByCategory",
+    ]),
+    isOpen: {
+      get() {
+        if (!this.$vuetify.breakpoint.mobile) return true
+        return this.$store.state.showFiltersDrawer
+      },
+      set(val) {
+        if (!this.$vuetify.breakpoint.mobile) return // you can't falsify isOpen on desktop
+        this.$store.state.showFiltersDrawer = val
+      },
+    },
+  },
+
+
+  methods: {
+    ...mapMutations([
+      "snackbar",
+      "openFacetsDialog",
+    ]),
+    ...mapActions([]),
+
+
+  },
+  created() {
+  },
+  mounted() {
+  },
+  watch: {
+    isOpen(to, from) {
+    }
+  }
 }
 </script>
 
