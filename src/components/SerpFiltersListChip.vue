389418a495062702e5109a8a21aa1dc81e11b323
Jason Priem <jason@ourresearch.org>
2023-04-21 09:44:08 -0700

    info menu for filter chips.

--- src/components/SerpFiltersListChip.vue
+++ src/components/SerpFiltersListChip.vue
@@ -1,42 +1,84 @@
 <template>
-  <v-chip large close
-          :text-color="myTextColor"
-          :disabled="disabled"
-
-          :color="myColor"
-          @click:close="remove"
-          @click="clickHandler"
-          class="ma-1 inline-flex py-2"
-          style="height: unset;  border: 1px solid !important; border-radius: 3px;"
-          close-icon="mdi-close"
+  <v-menu
+      offset-y
+      :close-on-content-click="false"
+      ref="menu"
   >
-<!--          style="height: unset; border-radius: 3px; border: 1px solid !important;"-->
+    <template v-slot:activator="{on}">
+      <v-chip large close
+              :text-color="myTextColor"
+              :disabled="disabled"
+              v-on="on"
 
-<!--    <v-icon>{{ filter.icon }}</v-icon>-->
-<!--    <span class="ml-1 mr-1 font-weight-bold">{{ filter.displayName }}: </span>-->
-<!--    <span>{{ myDisplayValue | truncate(50) }}</span>-->
+              :color="myColor"
+              @click:close="remove"
+              class="ma-1 inline-flex py-2"
+              style="height: unset;  border: 1px solid !important; border-radius: 3px;"
+              close-icon="mdi-close"
+      >
+        <!--              @click="clickHandler"-->
+        <!--          style="height: unset; border-radius: 3px; border: 1px solid !important;"-->
+
+        <!--    <v-icon>{{ filter.icon }}</v-icon>-->
+        <!--    <span class="ml-1 mr-1 font-weight-bold">{{ filter.displayName }}: </span>-->
+        <!--    <span>{{ myDisplayValue | truncate(50) }}</span>-->
 
 
-    <v-icon class="" style="font-size: 24px;">{{ filter.icon }}</v-icon>
-    <div class="mx-3" style="line-height: 1;">
-<!--      <div class="caption">-->
-<!--        <span class="font-weight-bold" v-if="isNegated">NOT</span>-->
-<!--        {{ filter.displayName }}-->
-<!--      </div>-->
-      <div class="filter-value" :class="{isNegated}">
+        <v-icon
+            class=""
+            style="font-size: 22px;"
+        >
+          {{ filter.icon }}
+        </v-icon>
+        <div class="display-name ml-2 font-weight-bold" v-if="filter.showNameInChip">{{ filter.displayName }}:</div>
+        <div class="mx-3" style="line-height: 1;">
+          <!--            <div class="caption">-->
+          <!--              <span class="font-weight-bold" v-if="isNegated">NOT</span>-->
+          <!--              {{ filter.displayName }}-->
+          <!--            </div>-->
+          <div class="filter-value" :class="{isNegated}">
         <span v-if="filter.valuesToShow==='search'" class="font-weight-bold">
           "{{ myDisplayValue | truncate(30) }}"
         </span>
-        <span v-else>
+            <span v-else class="">
         {{ myDisplayValue | truncate(30) }}
 
         </span>
+          </div>
+        </div>
+
+
+      </v-chip>
+    </template>
+    <v-card max-width="300">
+      <v-toolbar dense flat>
+        <v-toolbar-title>
+          <v-icon left>
+            {{ filter.icon }}
+          </v-icon>
+          <span class="">{{ filter.displayName }}</span>
+        </v-toolbar-title>
+          <v-spacer />
+          <v-btn icon @click="$refs.menu.save()">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+      </v-toolbar>
+      <div class="px-3">
+        <div>
+          {{ filter.displayValue }}
+        </div>
+        <div class="caption grey--text">
+          {{ filter.value }}
+        </div>
       </div>
-    </div>
-
-
-
-  </v-chip>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text small @click="remove">
+          Remove
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
 </template>
 
 <script>
@@ -49,7 +91,7 @@ export default {
   components: {},
   props: {
     filter: Object,
-      disabled: Boolean,
+    disabled: Boolean,
 
   },
   data() {
@@ -69,16 +111,16 @@ export default {
       }
       return this.filter.displayValue
     },
-    dynamicAttribute(){
+    dynamicAttribute() {
       return (this.filter.noOptions) ? null : "click"
     },
-    isNegated(){
+    isNegated() {
       return this.filter.isNegated
     },
-    myTextColor(){
+    myTextColor() {
       return (this.filter.isNegated) ? "red darken-2" : "green darken-3"
     },
-    myColor(){
+    myColor() {
       return (this.filter.isNegated) ? "red lighten-5" : "green lighten-5"
     }
   },
@@ -87,7 +129,7 @@ export default {
     ...mapMutations([
       "snackbar",
       "setFiltersZoom",
-        "setFacetZoom",
+      "setFacetZoom",
     ]),
     ...mapActions([]),
     remove() {
@@ -97,9 +139,9 @@ export default {
           newFilters
       )
     },
-      clickHandler(){
-        this.setFacetZoom(this.filter.key)
-      }
+    clickHandler() {
+      this.setFacetZoom(this.filter.key)
+    }
 
 
   },
@@ -115,7 +157,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .filter-value.isNegated {
-    //text-decoration: line-through;
-  }
+.filter-value.isNegated {
+  //text-decoration: line-through;
+}
 </style>
