19a7b048347802146e76ca2ea7b955ae96f9ce1a
Jason Priem <jp@jasonpriem.org>
2023-04-24 15:37:57 -0700

    clear facets column search when url changes.

--- src/components/SerpFacetsColumn.vue
+++ src/components/SerpFacetsColumn.vue
@@ -1,17 +1,17 @@
 <template>
   <v-card flat class="facets-column">
-    <v-toolbar flat >
-<!--      <v-text-field-->
-<!--            flat-->
-<!--            hide-details-->
-<!--            full-width-->
-<!--            clearable-->
-<!--            prepend-inner-icon="mdi-magnify"-->
-<!--            dense-->
+    <v-toolbar flat>
+      <!--      <v-text-field-->
+      <!--            flat-->
+      <!--            hide-details-->
+      <!--            full-width-->
+      <!--            clearable-->
+      <!--            prepend-inner-icon="mdi-magnify"-->
+      <!--            dense-->
 
-<!--            v-model="searchString"-->
-<!--            placeholder="Filters"-->
-<!--        />-->
+      <!--            v-model="searchString"-->
+      <!--            placeholder="Filters"-->
+      <!--        />-->
 
       <v-toolbar-title>
         <v-icon>mdi-filter-outline</v-icon>
@@ -21,23 +21,23 @@
 
       <template v-slot:extension class="pa-0 ">
         <v-text-field
-            flat
-            outlined
-            hide-details
-            full-width
-            clearable
-            prepend-inner-icon="mdi-magnify"
-            dense
-            color="green"
+                flat
+                outlined
+                hide-details
+                full-width
+                clearable
+                prepend-inner-icon="mdi-magnify"
+                dense
+                color="green"
 
-            v-model="searchString"
-            placeholder=""
+                v-model="searchString"
+                placeholder=""
         />
       </template>
     </v-toolbar>
     <v-list dense nav>
       <template
-          v-for="facetCategory in facetsByCategory(this.searchString)"
+              v-for="facetCategory in facetsByCategory(this.searchString)"
       >
         <v-subheader :key="'subheader'+facetCategory.name" class="mt-6">
           <v-icon small left>{{ facetCategory.icon }}</v-icon>
@@ -46,10 +46,10 @@
         <v-divider :key="'subheader-divider'+facetCategory.name"/>
 
         <facet-simple
-            v-for="facet in facetCategory.facets"
-            :key="facet.entityType + facet.key"
-            :facet-key="facet.key"
-            :facet-entity-type="entityType"
+                v-for="facet in facetCategory.facets"
+                :key="facet.entityType + facet.key"
+                :facet-key="facet.key"
+                :facet-entity-type="entityType"
         />
 
       </template>
@@ -66,62 +66,68 @@ import FacetSimple from "./Facet/FacetSimple.vue";
 import facet from "./Facet/Facet.vue";
 
 export default {
-  name: "SerpFacetsColumn",
-  components: {
-    FacetSimple,
-  },
-  props: {},
-  data() {
-    return {
-      searchString: "",
-    }
-  },
-  computed: {
-      facet() {
-          return facet
-      },
-    ...mapGetters([
-      "resultsFilters",
-      "entityType",
-      "facetsByCategory",
-    ]),
-    isOpen: {
-      get() {
-        if (!this.$vuetify.breakpoint.mobile) return true
-        return this.$store.state.showFiltersDrawer
-      },
-      set(val) {
-        if (!this.$vuetify.breakpoint.mobile) return // you can't falsify isOpen on desktop
-        this.$store.state.showFiltersDrawer = val
-      },
+    name: "SerpFacetsColumn",
+    components: {
+        FacetSimple,
     },
-  },
+    props: {},
+    data() {
+        return {
+            searchString: "",
+        }
+    },
+    computed: {
+        facet() {
+            return facet
+        },
+        ...mapGetters([
+            "resultsFilters",
+            "entityType",
+            "facetsByCategory",
+        ]),
+        isOpen: {
+            get() {
+                if (!this.$vuetify.breakpoint.mobile) return true
+                return this.$store.state.showFiltersDrawer
+            },
+            set(val) {
+                if (!this.$vuetify.breakpoint.mobile) return // you can't falsify isOpen on desktop
+                this.$store.state.showFiltersDrawer = val
+            },
+        },
+    },
 
 
-  methods: {
-    ...mapMutations([
-      "snackbar",
-      "openFacetsDialog",
-    ]),
-    ...mapActions([]),
+    methods: {
+        ...mapMutations([
+            "snackbar",
+            "openFacetsDialog",
+        ]),
+        ...mapActions([]),
 
 
-  },
-  created() {
-  },
-  mounted() {
-  },
-  watch: {
-    isOpen(to, from) {
+    },
+    created() {
+    },
+    mounted() {
+    },
+    watch: {
+        isOpen(to, from) {
+        },
+        "$route.query": {
+            immediate: true,
+            handler(newVal, oldVal) {
+                this.searchString = ""
+            }
+        },
     }
-  }
 }
 </script>
 
-<style  lang="scss">
+<style lang="scss">
 .facets-column .v-toolbar__extension {
-    padding-left: 7px !important;
-    padding-right: 7px !important;
-  }
+  padding-left: 7px !important;
+  padding-right: 7px !important;
+}
 
 </style>
