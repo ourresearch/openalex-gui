2ee3c81c11e4c797ca51d63fe74d6b75f73f108d
Jason Priem <jason@ourresearch.org>
2023-09-21 14:46:22 -0700

    fulltext edit works.

--- src/components/FilterEdit/FilterEditSearch.vue
+++ src/components/FilterEdit/FilterEditSearch.vue
@@ -1,26 +1,24 @@
 <template>
-  <v-card flat>
-    <v-toolbar flat dense>
-      <v-icon left>{{ myConfig.icon }}</v-icon>
-      <v-toolbar-title>
-        {{ myConfig.displayName }}
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-text>
+  <v-card rounded flat>
+    <v-toolbar flat>
+<!--      <v-icon left>{{ myConfig.icon }}</v-icon>-->
       <v-text-field
           autofocus
           v-model="myValue"
-          placeholder="Enter filter value"
+          :placeholder="myConfig.displayName"
           outlined
+          rounded
           hide-details
-          @keydown.enter="update"
+          @keyup.enter="$emit('upsert', myValue)"
       />
+    </v-toolbar>
+    <v-card-text>
     </v-card-text>
     <v-card-actions>
       <v-spacer/>
-      <v-btn text rounded @click="update">Cancel</v-btn>
-      <v-btn  rounded  color="primary" @click="update">
-        {{ createMode ? "Add filter" : "Update filter"}}
+      <v-btn text rounded @click="$emit('close')">Cancel</v-btn>
+      <v-btn  rounded  color="primary" @click="$emit('upsert', myValue)">
+        {{ createMode ? "Add search" : "Update search"}}
       </v-btn>
     </v-card-actions>
   </v-card>
