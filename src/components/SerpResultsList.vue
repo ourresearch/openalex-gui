6833fcccd4219e392c9ba51daa428dbebdfc3d62
Jason Priem <jp@jasonpriem.org>
2023-10-03 13:52:30 -0700

    sort works without vuex.

--- src/components/SerpResultsList.vue
+++ src/components/SerpResultsList.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="">
-
-
     <template v-if="resultsObject">
       <v-card flat dark class="ma-2" v-if="apiMode">
         <vue-json-pretty
@@ -16,50 +14,57 @@
         />
       </v-card>
       <v-card flat v-else>
-        <div class="mx-4">
-          <div v-if="!resultsCount" class="mt-8 grey--text">
-            There are no results for this search.
+        <div v-if="!resultsCount" class="mt-8 grey--text">
+          There are no results for this search.
+        </div>
+        <v-toolbar v-else dense flat class="">
+          <div class="grey--text">
+            {{ resultsCount | toPrecision }} results
           </div>
-          <div v-else class="d-flex">
-            <div class="mt-4 grey--text">
-              {{ resultsCount | toPrecision }} results
-            </div>
-            <v-spacer/>
-            <v-menu>
-              <template v-slot:activator="{on}">
-                <v-btn icon class="font-weight-regular" text v-on="on">
-                  <v-icon>mdi-sort</v-icon>
-                  <v-icon v-if="$vuetify.breakpoint.mobile">mdi-sort</v-icon>
-                  <template v-else>
-                    <v-icon left class="">mdi-sort</v-icon>
-                    Sort by {{ sortObject.displayName }}
-                  </template>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-subheader>Sort by</v-subheader>
-                <v-divider></v-divider>
+          <v-spacer/>
+          <v-menu>
+            <template v-slot:activator="{on}">
+              <v-btn
+                  :icon="$vuetify.breakpoint.mobile"
+                  text
+                  rounded
+                  class="font-weight-regular"
+                  v-on="on"
+              >
+                <v-icon v-if="$vuetify.breakpoint.mobile">mdi-sort</v-icon>
+                <template v-else>
+                  <v-icon left class="">mdi-sort</v-icon>
+                  Sort by {{ activeSortConfig.displayName }}
+                </template>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-subheader>Sort by</v-subheader>
+              <v-divider></v-divider>
+              <v-list-item-group
+                  mandatory
+                  v-model="activeSortKey"
+              >
                 <v-list-item
-                    v-for="mySortOption in sortObjectOptions"
-                    :key="mySortOption.key"
-                    @click="setSort(mySortOption.key)"
+                    v-for="sortConfig in sortObjectOptions"
+                    :key="sortConfig.key"
+                    :value="sortConfig.key"
                 >
                   <v-list-item-icon>
-                    <v-icon>
-                      {{ (sortObject.key === mySortOption.key) ? "mdi-radiobox-marked" : "mdi-radiobox-blank" }}
-                    </v-icon>
+                      <v-icon v-if="activeSortKey === sortConfig.key">mdi-check</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
                     <v-list-item-title>
-                      {{ mySortOption.displayName }}
+                      {{ sortConfig.displayName }}
                     </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
-              </v-list>
-            </v-menu>
 
-          </div>
-        </div>
+              </v-list-item-group>
+            </v-list>
+          </v-menu>
+
+        </v-toolbar>
         <v-list v-if="resultsCount" class="serp-results-list" nav>
           <component
               v-for="result in resultsObject.results"
@@ -101,6 +106,7 @@ import ResultFunder from "@/components/Result/ResultFunder.vue";
 import ResultInstitution from "./Result/ResultInstitution.vue";
 import ResultConcept from "./Result/ResultConcept.vue";
 import {entityTypes} from "../util";
+import router from "../router";
 
 
 const sortConfigs = [
@@ -132,35 +138,35 @@ const sortConfigs = [
 
 
 const sortDefaults = {
-    works: {
-        textSearch: "relevance_score:desc",
-        // noTextSearch: "publication_date:desc",
-        noTextSearch: "cited_by_count:desc",
-    },
-    authors: {
-        textSearch: "relevance_score:desc",
-        noTextSearch: "works_count:desc",
-    },
-    sources: {
-        textSearch: "relevance_score:desc",
-        noTextSearch: "works_count:desc",
-    },
-    publishers: {
-        textSearch: "relevance_score:desc",
-        noTextSearch: "works_count:desc",
-    },
-    funders: {
-        textSearch: "relevance_score:desc",
-        noTextSearch: "works_count:desc",
-    },
-    institutions: {
-        textSearch: "relevance_score:desc",
-        noTextSearch: "works_count:desc",
-    },
-    concepts: {
-        textSearch: "relevance_score:desc",
-        noTextSearch: "works_count:desc",
-    },
+  works: {
+    textSearch: "relevance_score:desc",
+    // noTextSearch: "publication_date:desc",
+    noTextSearch: "cited_by_count:desc",
+  },
+  authors: {
+    textSearch: "relevance_score:desc",
+    noTextSearch: "works_count:desc",
+  },
+  sources: {
+    textSearch: "relevance_score:desc",
+    noTextSearch: "works_count:desc",
+  },
+  publishers: {
+    textSearch: "relevance_score:desc",
+    noTextSearch: "works_count:desc",
+  },
+  funders: {
+    textSearch: "relevance_score:desc",
+    noTextSearch: "works_count:desc",
+  },
+  institutions: {
+    textSearch: "relevance_score:desc",
+    noTextSearch: "works_count:desc",
+  },
+  concepts: {
+    textSearch: "relevance_score:desc",
+    noTextSearch: "works_count:desc",
+  },
 }
 
 export default {
@@ -183,8 +189,7 @@ export default {
   data() {
     return {
       resultsPerPage: 25, // not editable now, but could be in future
-      isTextSearch: false,
-      sort: "cited_by_count:desc",
+      // activeSortKey: "cited_by_count:desc",
     }
   },
   computed: {
@@ -192,8 +197,24 @@ export default {
       "entityConfig",
       "entityType",
     ]),
+    activeSortKey: {
+      get(){
+        const fromUrl = this.$route.query.sort
+        const fromDefault = this.defaultSort
+        return fromUrl ?? fromDefault
+      },
+      set(val){
+        this.pushQueryChanges({
+          sort: val
+        })
+      }
+    },
     resultComponentName() {
       return "result-" + this.entityConfig.nameSingular
+    },
+    isSearchFilterApplied() {
+      const params = new URLSearchParams(this.$route.query)
+      return params.toString().indexOf(".search") > -1
     },
     resultsCount() {
       return this.resultsObject.meta.count
@@ -213,21 +234,20 @@ export default {
       )
     },
     defaultSort() {
-      return sortDefaults[this.entityType][(this.isTextSearch) ? "textSearch" : "noTextSearch"]
+      return sortDefaults[this.entityType][(this.isSearchFilterApplied) ? "textSearch" : "noTextSearch"]
     },
 
     sortObjectOptions() {
-      return sortConfigs
-      if (!this.resultsCount) return
+      // if (!this.resultsCount) return
       return sortConfigs.filter(sortConfig => {
-        if (sortConfig.requiresTextSearch && !this.isTextSearch) return false
+        if (sortConfig.requiresTextSearch && !this.isSearchFilterApplied) return false
         if (!sortConfig.showForEntityTypes.includes(this.entityType)) return false
         return true
       })
     },
-    sortObject() {
-      return sortConfigs.find(sortOption => {
-        return sortOption.key === this.sort
+    activeSortConfig() {
+      return sortConfigs.find(sortConfig => {
+        return sortConfig.key === this.activeSortKey
       })
     },
 
@@ -241,11 +261,25 @@ export default {
     ...mapActions([
       "setSort",
     ]),
+    async pushQueryChanges(query) {
 
-    setSort(sortKey) {
-      const mySortConfig = sortConfigs.find(c => c.key === sortKey)
-      if (mySortConfig) this.sort = mySortConfig.key
-    },
+      const pushTo = {
+        name: "Serp",
+        query: {
+          ...this.$route.query,
+          ...query,
+        }
+      }
+      console.log("pushQueryChanges", query)
+      await this.$router.push(pushTo)
+          .catch((e) => {
+            if (e.name !== "NavigationDuplicated") {
+              throw e
+            }
+          })
+
+    }
+
 
 
   },
@@ -255,7 +289,15 @@ export default {
   },
   watch: {
     isOpen(to, from) {
-    }
+    },
+    // "$route": {
+    //   immediate: true,
+    //   handler(to){
+    //     if (this.isSearchFilterApplied && this.activeSortConfig.requiresTextSearch) {
+    //       this.activeSortKey = this.defaultSort
+    //     }
+    //   }
+    // }
   }
 }
 </script>
