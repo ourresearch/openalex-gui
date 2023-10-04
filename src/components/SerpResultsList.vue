dc0812f4bbfede6d6b29cec303ec2382518be3d4
Jason Priem <jp@jasonpriem.org>
2023-10-03 18:27:53 -0700

    bigger add-group button.

--- src/components/SerpResultsList.vue
+++ src/components/SerpResultsList.vue
@@ -22,47 +22,6 @@
             {{ resultsCount | toPrecision }} results
           </div>
           <v-spacer/>
-          <v-menu>
-            <template v-slot:activator="{on}">
-              <v-btn
-                  :icon="$vuetify.breakpoint.mobile"
-                  text
-                  rounded
-                  class="font-weight-regular"
-                  v-on="on"
-              >
-                <v-icon v-if="$vuetify.breakpoint.mobile">mdi-sort</v-icon>
-                <template v-else>
-                  <v-icon left class="">mdi-sort</v-icon>
-                  Sort by {{ activeSortConfig.displayName }}
-                </template>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-subheader>Sort by</v-subheader>
-              <v-divider></v-divider>
-              <v-list-item-group
-                  mandatory
-                  v-model="activeSortKey"
-              >
-                <v-list-item
-                    v-for="sortConfig in sortObjectOptions"
-                    :key="sortConfig.key"
-                    :value="sortConfig.key"
-                >
-                  <v-list-item-icon>
-                      <v-icon v-if="activeSortKey === sortConfig.key">mdi-check</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      {{ sortConfig.displayName }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-
-              </v-list-item-group>
-            </v-list>
-          </v-menu>
 
         </v-toolbar>
         <v-list v-if="resultsCount" class="serp-results-list" nav>
@@ -109,65 +68,6 @@ import {entityTypes} from "../util";
 import router from "../router";
 
 
-const sortConfigs = [
-  {
-    key: "cited_by_count:desc",
-    displayName: "Citations",
-    showForEntityTypes: entityTypes.all(),
-  },
-  {
-    // only for non-work entities
-    key: "works_count:desc",
-    displayName: "Works",
-    showForEntityTypes: entityTypes.allExcept("works"),
-  },
-  {
-    // only for works
-    key: "publication_date:desc",
-    displayName: "Date",
-    showForEntityTypes: ["works"],
-  },
-  {
-    // only if there's a text search on
-    key: "relevance_score:desc",
-    displayName: "Relevance",
-    showForEntityTypes: entityTypes.all(),
-    requiresTextSearch: true,
-  },
-]
-
-
-const sortDefaults = {
-  works: {
-    textSearch: "relevance_score:desc",
-    // noTextSearch: "publication_date:desc",
-    noTextSearch: "cited_by_count:desc",
-  },
-  authors: {
-    textSearch: "relevance_score:desc",
-    noTextSearch: "works_count:desc",
-  },
-  sources: {
-    textSearch: "relevance_score:desc",
-    noTextSearch: "works_count:desc",
-  },
-  publishers: {
-    textSearch: "relevance_score:desc",
-    noTextSearch: "works_count:desc",
-  },
-  funders: {
-    textSearch: "relevance_score:desc",
-    noTextSearch: "works_count:desc",
-  },
-  institutions: {
-    textSearch: "relevance_score:desc",
-    noTextSearch: "works_count:desc",
-  },
-  concepts: {
-    textSearch: "relevance_score:desc",
-    noTextSearch: "works_count:desc",
-  },
-}
 
 export default {
   name: "SerpResultsList",
@@ -197,24 +97,8 @@ export default {
       "entityConfig",
       "entityType",
     ]),
-    activeSortKey: {
-      get(){
-        const fromUrl = this.$route.query.sort
-        const fromDefault = this.defaultSort
-        return fromUrl ?? fromDefault
-      },
-      set(val){
-        this.pushQueryChanges({
-          sort: val
-        })
-      }
-    },
     resultComponentName() {
       return "result-" + this.entityConfig.nameSingular
-    },
-    isSearchFilterApplied() {
-      const params = new URLSearchParams(this.$route.query)
-      return params.toString().indexOf(".search") > -1
     },
     resultsCount() {
       return this.resultsObject.meta.count
@@ -233,23 +117,7 @@ export default {
           10
       )
     },
-    defaultSort() {
-      return sortDefaults[this.entityType][(this.isSearchFilterApplied) ? "textSearch" : "noTextSearch"]
-    },
 
-    sortObjectOptions() {
-      // if (!this.resultsCount) return
-      return sortConfigs.filter(sortConfig => {
-        if (sortConfig.requiresTextSearch && !this.isSearchFilterApplied) return false
-        if (!sortConfig.showForEntityTypes.includes(this.entityType)) return false
-        return true
-      })
-    },
-    activeSortConfig() {
-      return sortConfigs.find(sortConfig => {
-        return sortConfig.key === this.activeSortKey
-      })
-    },
 
 
   },
@@ -259,26 +127,7 @@ export default {
       "snackbar",
     ]),
     ...mapActions([
-      "setSort",
     ]),
-    async pushQueryChanges(query) {
-
-      const pushTo = {
-        name: "Serp",
-        query: {
-          ...this.$route.query,
-          ...query,
-        }
-      }
-      console.log("pushQueryChanges", query)
-      await this.$router.push(pushTo)
-          .catch((e) => {
-            if (e.name !== "NavigationDuplicated") {
-              throw e
-            }
-          })
-
-    }
 
 
 
@@ -288,16 +137,6 @@ export default {
   mounted() {
   },
   watch: {
-    isOpen(to, from) {
-    },
-    // "$route": {
-    //   immediate: true,
-    //   handler(to){
-    //     if (this.isSearchFilterApplied && this.activeSortConfig.requiresTextSearch) {
-    //       this.activeSortKey = this.defaultSort
-    //     }
-    //   }
-    // }
   }
 }
 </script>
