63ca3f505bc88c1c88a2f74d76d3c945f353b407
Jason Priem <jason@ourresearch.org>
2023-10-11 15:03:15 -0700

    resolve git nonsense.

--- src/views/Serp.vue
+++ src/views/Serp.vue
@@ -18,12 +18,6 @@
       <serp-api-editor
           class="mb-3"
       />
-<!--      <div class="d-flex">-->
-<!--        <v-spacer/>-->
-<!--        <v-btn icon @click="apiMode = !apiMode">-->
-<!--          <v-icon>mdi-api</v-icon>-->
-<!--        </v-btn>-->
-<!--      </div>-->
       <filter-chips-list />
 
       <v-row dense>
