84df456b9818646a917ea607d1532c0bbaca874f
Jason Priem <jason@ourresearch.org>
2024-02-27 18:57:55 +0100

    prettier saved searches.

--- src/views/SavedSearches.vue
+++ src/views/SavedSearches.vue
@@ -1,123 +1,127 @@
 <template>
-  <v-container>
-    <v-toolbar flat>
-<!--      <v-btn icon @click="$router.back()">-->
-<!--        <v-icon>mdi-arrow-left</v-icon>-->
-<!--      </v-btn>-->
-      <v-toolbar-title class="text-h6">
-        Your saved searches
+  <div class="color-2 saved-searches-page" style="min-height: 50vh;">
+    <v-container class="py-12">
+      <v-card rounded flat class="px-2 pb-4">
+      <v-toolbar flat>
+        <!--      <v-btn icon @click="$router.back()">-->
+        <!--        <v-icon>mdi-arrow-left</v-icon>-->
+        <!--      </v-btn>-->
+        <v-toolbar-title class="font-weight-bold">
+          Your saved searches
+          <span class="ml-2 font-weight-regular">({{ userSavedSearches?.length ?? 0 }})</span>
+        </v-toolbar-title>
+        <v-spacer/>
+<!--        <v-btn rounded color="primary" :to="{name:'Serp', params: {entityType: 'works'}}">-->
+<!--          <v-icon left>mdi-plus</v-icon>-->
+<!--          new search-->
+<!--        </v-btn>-->
+      </v-toolbar>
+      <v-simple-table v-if="userSavedSearches.length">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Last updated</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="(savedSearch, i) in userSavedSearches"
+            :key="savedSearch.id"
+            @click="openSavedSearch(savedSearch.id)"
+            class="saved-search-row"
+        >
+          <td>
+            <v-icon left>mdi-folder-outline</v-icon>
+            {{ savedSearch.name }}
+          </td>
+          <td>
+            {{ formatDate(savedSearch.updated) }}
+            <!--          {{ (savedSearch.updated) }} -->
+          </td>
+          <td class="d-flex align-center">
+            <v-spacer></v-spacer>
+            <v-btn icon @click.stop="setEditAlertId(savedSearch.id)">
+              <v-icon>{{ savedSearch.has_alert ? "mdi-bell" : "mdi-bell-outline" }}</v-icon>
+            </v-btn>
+            <v-menu offset-y>
+              <template v-slot:activator="{on}">
+                <v-btn icon v-on="on">
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
+              <saved-search-menu :id="savedSearch.id"/>
 
-      </v-toolbar-title>
-      <v-spacer/>
-      <v-btn rounded color="primary" :to="{name:'Serp', params: {entityType: 'works'}}">
-        <v-icon left>mdi-plus</v-icon>
-        new search
-      </v-btn>
-    </v-toolbar>
-    <v-simple-table v-if="userSavedSearches.length">
-      <thead>
-      <tr>
-        <th>Name</th>
-        <th>Last updated</th>
-        <th></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr
-          v-for="(savedSearch, i) in userSavedSearches"
-          :key="savedSearch.id"
-          @click="openSavedSearch(savedSearch.id)"
-          class="saved-search-row"
-      >
-        <td>
-          <v-icon left>mdi-folder-outline</v-icon>
-          {{ savedSearch.name }}
-        </td>
-        <td>
-          {{ formatDate(savedSearch.updated) }}
-          <!--          {{ (savedSearch.updated) }} -->
-        </td>
-        <td class="d-flex align-center">
-          <v-spacer></v-spacer>
-          <v-btn icon @click.stop="setEditAlertId(savedSearch.id)">
-            <v-icon>{{ savedSearch.has_alert ? "mdi-bell" : "mdi-bell-outline" }}</v-icon>
-          </v-btn>
-          <v-menu offset-y>
-            <template v-slot:activator="{on}">
-              <v-btn icon v-on="on">
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
-            <saved-search-menu :id="savedSearch.id"/>
+              <!--            <v-list>-->
+              <!--              <v-list-item @click="openRenameDialog(savedSearch.id)">-->
+              <!--                <v-list-item-icon>-->
+              <!--                  <v-icon>mdi-pencil-outline</v-icon>-->
+              <!--                </v-list-item-icon>-->
+              <!--                <v-list-item-title>Rename</v-list-item-title>-->
+              <!--              </v-list-item>-->
+              <!--              <v-list-item @click="deleteSavedSearch(savedSearch.id)">-->
+              <!--                <v-list-item-icon>-->
+              <!--                  <v-icon>mdi-delete-outline</v-icon>-->
+              <!--                </v-list-item-icon>-->
+              <!--                <v-list-item-title>Delete</v-list-item-title>-->
+              <!--              </v-list-item>-->
+              <!--              <v-list-item @click="openAsCopy(savedSearch.id)">-->
+              <!--                <v-list-item-icon>-->
+              <!--                  <v-icon>mdi-folder-multiple-outline</v-icon>-->
+              <!--                </v-list-item-icon>-->
+              <!--                <v-list-item-title>Open as copy</v-list-item-title>-->
+              <!--              </v-list-item>-->
+              <!--            </v-list>-->
+            </v-menu>
+          </td>
 
-            <!--            <v-list>-->
-            <!--              <v-list-item @click="openRenameDialog(savedSearch.id)">-->
-            <!--                <v-list-item-icon>-->
-            <!--                  <v-icon>mdi-pencil-outline</v-icon>-->
-            <!--                </v-list-item-icon>-->
-            <!--                <v-list-item-title>Rename</v-list-item-title>-->
-            <!--              </v-list-item>-->
-            <!--              <v-list-item @click="deleteSavedSearch(savedSearch.id)">-->
-            <!--                <v-list-item-icon>-->
-            <!--                  <v-icon>mdi-delete-outline</v-icon>-->
-            <!--                </v-list-item-icon>-->
-            <!--                <v-list-item-title>Delete</v-list-item-title>-->
-            <!--              </v-list-item>-->
-            <!--              <v-list-item @click="openAsCopy(savedSearch.id)">-->
-            <!--                <v-list-item-icon>-->
-            <!--                  <v-icon>mdi-folder-multiple-outline</v-icon>-->
-            <!--                </v-list-item-icon>-->
-            <!--                <v-list-item-title>Open as copy</v-list-item-title>-->
-            <!--              </v-list-item>-->
-            <!--            </v-list>-->
-          </v-menu>
-        </td>
-
-      </tr>
-      </tbody>
-    </v-simple-table>
-    <v-card rounded flat class="color-3 d-flex my-12 mx-4 pa-12" v-else>
-      <div class="grey--text">
-        You have no saved searches.
-      </div>
-
-    </v-card>
-
-    <!--    <v-list rounded>-->
-    <!--      <user-saved-search-->
-    <!--          v-for="(savedSearch, i) in userSavedSearches"-->
-    <!--          :key="savedSearch.id"-->
-    <!--          :id="savedSearch.id"-->
-    <!--          :search-url="savedSearch.search_url"-->
-    <!--          :updated="savedSearch.updated"-->
-    <!--      >-->
-    <!--      </user-saved-search>-->
-    <!--    </v-list>-->
-
-    <v-dialog v-model="isDialogOpen.rename" max-width="600">
-      <v-card flat rounded>
-        <v-card-title>Rename saved search</v-card-title>
-        <div class="pa-4">
-          <v-text-field
-              autofocus
-              rounded
-              filled
-              hide-details
-              clearable
-              prepend-inner-icon="mdi-magnify"
-              placeholder="New name"
-              v-model="renameString"
-          />
+        </tr>
+        </tbody>
+      </v-simple-table>
+      <div  class="color-3 d-flex my-12 mx-4 pa-12" v-else>
+        <div class="grey--text">
+          You have no saved searches.
         </div>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn text rounded @click="isDialogOpen.rename = false">Cancel</v-btn>
-          <v-btn text rounded color="primary" @click="rename(searchIdToRename, renameString)">Rename</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
 
-  </v-container>
+      </div>
+      </v-card>
+
+      <!--    <v-list rounded>-->
+      <!--      <user-saved-search-->
+      <!--          v-for="(savedSearch, i) in userSavedSearches"-->
+      <!--          :key="savedSearch.id"-->
+      <!--          :id="savedSearch.id"-->
+      <!--          :search-url="savedSearch.search_url"-->
+      <!--          :updated="savedSearch.updated"-->
+      <!--      >-->
+      <!--      </user-saved-search>-->
+      <!--    </v-list>-->
+
+      <v-dialog v-model="isDialogOpen.rename" max-width="600">
+        <v-card flat rounded>
+          <v-card-title>Rename saved search</v-card-title>
+          <div class="pa-4">
+            <v-text-field
+                autofocus
+                rounded
+                filled
+                hide-details
+                clearable
+                prepend-inner-icon="mdi-magnify"
+                placeholder="New name"
+                v-model="renameString"
+            />
+          </div>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn text rounded @click="isDialogOpen.rename = false">Cancel</v-btn>
+            <v-btn text rounded color="primary" @click="rename(searchIdToRename, renameString)">Rename</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+    </v-container>
+  </div>
 
 
 </template>
@@ -240,10 +244,15 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.saved-search-row {
-  cursor: pointer;
-}
+<style lang="scss" >
+.saved-searches-page {
+  .saved-search-row {
+    cursor: pointer;
+  }
+  table {
+    border-top: none !important;
+  }
 
+}
 
 </style>
