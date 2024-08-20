0607f6584e29329215962d13e56cdb8c749fa0b0
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-20 08:24:39 -0500

    Account for different operator and/or, handle column_ids

--- src/oqlParse/oqlParse.js
+++ src/oqlParse/oqlParse.js
@@ -1,12 +1,22 @@
+
+const COLUMN_IDS_MAP = {
+    'institution': "authorships.institutions.id",
+    "funder": "grants.funder",
+    "name": "default.search"
+};
+
 function generateId(prefix = "leaf") {
     return `${prefix}_${Math.random().toString(36).substr(2, 8)}`;
 }
 
 function parseCondition(condition, subjectEntity) {
-    let match = condition.match(/(\S+)\s+(is not|is)\s+(.+)/);
+    let match = condition.match(/(\S+)\s+(is not|is|contains|does not contain)\s+(.+)/);
     if (match) {
         let column_id = match[1];
-        let operator = match[2] === 'is' ? 'is' : 'is not';
+        if (column_id in COLUMN_IDS_MAP) {
+            column_id = COLUMN_IDS_MAP[column_id];
+        }
+        let operator = match[2];
         let value = match[3].replace(/;/g, '').trim();
 
         if (value.match(/^\d+$/)) {
@@ -32,16 +42,27 @@ function parseFilters(oql) {
     const worksConditions = [];
     let summarizeByCondition = null;
     let summarizeByEntity = null;
+    let worksOperator = "and";
+    let summarizeByOperator = "and";
 
     const worksMatch = oql.match(/where (.+?)(;|$)/);
     if (worksMatch) {
-        worksConditions.push(...worksMatch[1].split(" and "));
+        const worksClause = worksMatch[1];
+        if (worksClause.includes(" or ")) {
+            worksOperator = "or";
+            worksConditions.push(...worksClause.split(" or "));
+        } else {
+            worksConditions.push(...worksClause.split(" and "));
+        }
     }
 
-    const summarizeByMatch = oql.match(/summarize by (\w) where (.+?)(;|$)/);
+    const summarizeByMatch = oql.match(/summarize by (\w+) where (.+?)(;|$)/);
     if (summarizeByMatch) {
         summarizeByEntity = summarizeByMatch[1];
         summarizeByCondition = summarizeByMatch[2];
+        if (summarizeByCondition.includes(" or ")) {
+            summarizeByOperator = "or";
+        }
     }
 
     const worksBranchId = generateId("br");
@@ -60,7 +81,7 @@ function parseFilters(oql) {
             id: worksBranchId,
             subjectEntity: "works",
             type: "branch",
-            operator: "and",
+            operator: worksOperator,
             children: worksBranchChildren,
             isRoot: true
         });
@@ -68,15 +89,19 @@ function parseFilters(oql) {
 
     if (summarizeByMatch) {
         const summarizeByBranchId = generateId("br");
-        const summarizeByLeaf = parseCondition(summarizeByCondition, summarizeByEntity);
-        if (summarizeByLeaf) {
-            filters.push(summarizeByLeaf);
+        const summarizeByLeaves = summarizeByCondition.split(summarizeByOperator === "or" ? " or " : " and ")
+            .map(condition => parseCondition(condition.trim(), summarizeByEntity))
+            .filter(leaf => leaf !== null);
+
+        filters.push(...summarizeByLeaves);
+
+        if (summarizeByLeaves.length > 0) {
             filters.push({
                 id: summarizeByBranchId,
                 subjectEntity: summarizeByEntity,
                 type: "branch",
-                operator: "and",
-                children: [summarizeByLeaf.id],
+                operator: summarizeByOperator,
+                children: summarizeByLeaves.map(leaf => leaf.id),
                 isRoot: true
             });
         }
@@ -115,8 +140,12 @@ function oqlToQuery(oql) {
     if (oql.includes("sort by")) {
         const sortByMatch = oql.match(/sort by (\w+) (asc|desc)/);
         if (sortByMatch) {
+            let column_id = sortByMatch[1];
+            if (column_id in COLUMN_IDS_MAP) {
+                column_id = COLUMN_IDS_MAP[column_id];
+            }
             query.sort_by = {
-                column_id: sortByMatch[1],
+                column_id: column_id,
                 direction: sortByMatch[2]
             };
         }
