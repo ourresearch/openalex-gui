d5289931a51be5258e50844b9fbbd8191eb9765c
Jason Priem <jason@ourresearch.org>
2024-08-23 14:34:58 +0200

    update order of filter dicts.

--- src/components/Query/query.js
+++ src/components/Query/query.js
@@ -3,23 +3,20 @@ const makeFilterLeaf = function(subjectEntity){
     return {
         id: "leaf_" + shortUUID.generate().slice(0,6),
         subjectEntity,
-        operator: null,
         type: "leaf",
 
-        // leafy properties
         column_id: null,
+        operator: null,
         value: null,
-        // children: [],
     }
 }
 const makeFilterBranch = function(subjectEntity){
     return {
         id: "br_" + shortUUID.generate().slice(0,6),
         subjectEntity,
-        operator: "and",  // can be either "and" or "or"
         type: "branch",
 
-        // branchy properties
+        operator: "and",  // can be either "and" or "or"
         children: [],
     }
 }
