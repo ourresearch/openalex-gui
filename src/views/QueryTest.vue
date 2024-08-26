a4e271a6da7873a29fb159ceec869a8094463586
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-26 09:42:17 -0500

    Test UI update

--- src/views/QueryTest.vue
+++ src/views/QueryTest.vue
@@ -7,18 +7,18 @@
 
       <v-card-text>
         <v-row justify="center" align="center">
-          <v-col cols="12" sm="8" md="6">
+          <v-col cols="12" sm="6" md="5">
             <v-select
               v-model="selectedTests"
               :items="availableTests"
-              label="Select tests to run"
+              label="Select cases to run"
               multiple
               chips
               persistent-hint
-              hint="Choose one or more tests"
+              hint="Choose one or more cases"
             ></v-select>
           </v-col>
-          <v-col cols="12" sm="4" md="2">
+          <v-col cols="12" sm="3" md="2">
             <v-btn
               color="primary"
               block
@@ -30,51 +30,127 @@
           </v-col>
         </v-row>
 
-        <v-data-table
-          v-if="showResults"
-          :headers="headers"
-          :items="testResults"
-          class="elevation-1 mt-4"
-        ></v-data-table>
+    <v-data-table
+      :headers="headers"
+      :items="tableItems"
+      class="elevation-1 mt-4"
+    >
+      <template v-slot:item.test="{ item }">
+        <a href="#" @click.prevent="showTestObject(item.id)">{{ item.id }}</a>
+      </template>
+      <template v-slot:item.natLangToJson="{ item }">
+        <v-icon :color="item.natlangToJson === true ? 'green' : item.natlangToJson === false ? 'red' : 'grey'">
+          {{ item.natlangToJson === true ? 'mdi-check' : item.natlangToJson === false ? 'mdi-close' : 'mdi-minus' }}
+        </v-icon>
+      </template>
+      <template v-slot:item.oqlToJson="{ item }">
+        <v-icon :color="item.oqlToJson === true ? 'green' : item.oqlToJson === false ? 'red' : 'grey'">
+          {{ item.oqlToJson === true ? 'mdi-check' : item.oqlToJson === false ? 'mdi-close' : 'mdi-minus' }}
+        </v-icon>
+      </template>
+      <template v-slot:item.jsonToOql="{ item }">
+        <v-icon :color="item.jsonToOql === true ? 'green' : item.jsonToOql === false ? 'red' : 'grey'">
+          {{ item.jsonToOql === true ? 'mdi-check' : item.jsonToOql === false ? 'mdi-close' : 'mdi-minus' }}
+        </v-icon>
+      </template>
+      <template v-slot:item.jsonToSearch="{ item }">
+        <v-icon :color="item.jsonToSearch === true ? 'green' : item.jsonToSearch === false ? 'red' : 'grey'">
+          {{ item.jsonToSearch === true ? 'mdi-check' : item.jsonToSearch === false ? 'mdi-close' : 'mdi-minus' }}
+        </v-icon>
+      </template>
+    </v-data-table>
       </v-card-text>
     </v-card>
+
+    <v-dialog v-model="dialog" max-width="800px">
+      <v-card>
+        <v-card-title>
+          Test Object
+          <v-spacer></v-spacer>
+          <v-btn icon @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <pre>{{ JSON.stringify(selectedTestObject, null, 2) }}</pre>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
   </v-container>
 </template>
 
 <script>
+import { OQOTestRunner, getTests } from '@/oqlParse/test';
+import { objectMD5, invertMap } from '@/oqlParse/util';
+
 export default {
   name: "OQOTests",
   data() {
     return {
+      tests: [],
       selectedTests: [],
       availableTests: [
-        { text: 'natlangToJson', value: 'natlangToJson' },
+        { text: 'natLangToJson', value: 'natLangToJson' },
         { text: 'oqlToJson', value: 'oqlToJson' },
         { text: 'jsonToOql', value: 'jsonToOql' },
         { text: 'jsonToSearch', value: 'jsonToSearch' },
       ],
-      showResults: false,
       headers: [
-        { text: 'natlangToJson', value: 'natlangToJson' },
+        { text: 'Test', value: 'test' },
+        { text: 'natLangToJson', value: 'natLangToJson' },
         { text: 'oqlToJson', value: 'oqlToJson' },
         { text: 'jsonToOql', value: 'jsonToOql' },
         { text: 'jsonToSearch', value: 'jsonToSearch' },
       ],
-      testResults: [],
+      tableItems: [],
+      dialog: false,
+      selectedTestObject: null,
+      testCasesMap: {
+        'natLangToJson': 'natLang',
+        'oqlToJson': 'oqlToQuery',
+        'jsonToOql': 'queryToOql',
+        'jsonToSearch': 'queryToSearch'
+      },
     }
   },
+  created() {
+    this.loadTests();
+  },
   methods: {
-    runTests() {
-      this.testResults = [
-        {
-          natlangToJson: this.selectedTests.includes('natlangToJson') ? 'Result 1' : '',
-          oqlToJson: this.selectedTests.includes('oqlToJson') ? 'Result 2' : '',
-          jsonToOql: this.selectedTests.includes('jsonToOql') ? 'Result 3' : '',
-          jsonToSearch: this.selectedTests.includes('jsonToSearch') ? 'Result 4' : '',
-        },
-      ];
-      this.showResults = true;
+    loadTests() {
+      getTests().then(tests => {
+        this.tests = tests;
+        this.tableItems = this.tests.map(test => ({
+          id: objectMD5(test),
+          test: test,
+          natLangToJson: '',
+          oqlToJson: '',
+          jsonToOql: '',
+          jsonToSearch: '',
+        }));
+      });
     },
+    async runTests() {
+      const runner = new OQOTestRunner(this.tests, this.updateTestResult);
+      await runner.runTests(this.selectedTests.map(test => this.testCasesMap[test] || test));
+    },
+    updateTestResult(testResult) {
+      const rowIndex = this.tableItems.findIndex(item => item.id === testResult.id);
+      if (rowIndex !== -1) {
+        const tableKey = invertMap(this.testCasesMap)[testResult.case];
+        this.tableItems[rowIndex][tableKey] = testResult.isPassing;
+      } else {
+        console.error('No matching row found for test result:', testResult);
+      }
+    },
+    showTestObject(id) {
+      const item = this.tableItems.find(item => item.id === id);
+      if (item) {
+        this.selectedTestObject = item.test;
+        this.dialog = true;
+      }
+    }
   },
 }
 </script>
