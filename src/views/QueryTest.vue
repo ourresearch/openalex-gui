29e784c1639f0b773b4102da8e0958a83721af74
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-28 23:20:15 -0500

    Display modal with test result details and full test object in 2 panes, left and right

--- src/views/QueryTest.vue
+++ src/views/QueryTest.vue
@@ -60,7 +60,7 @@
                   Clear
                 </v-btn>
               </v-col>
-        </v-row>
+            </v-row>
           </v-col>
         </v-row>
 
@@ -76,7 +76,13 @@
             :items-per-page="-1"
         >
           <template v-slot:item.test="{ item }">
-            <span>{{ item.test.oql }}</span>
+  <span
+      class="primary--text text-decoration-underline"
+      style="cursor: pointer;"
+      @click="showTestObject(item.id)"
+  >
+    {{ item.test.oql }}
+  </span>
           </template>
           <template v-slot:item.tags="{ item }">
             <v-chip
@@ -90,32 +96,59 @@
           </template>
           <template v-slot:item.natLangToJson="{ item }">
             <nat-lang-to-json-cell :value="item.natLangToJson"
+                                   :testObject="item.test"
+                                   @show-details="showTestDetails"
                                    :loading="isLoadingCell(item.id, 'natLang')"/>
           </template>
           <template v-slot:item.oqlToJson="{ item }">
             <test-result-cell
                 :value="item.oqlToJson.isPassing"
                 :loading="isLoadingCell(item.id, 'oqlToQuery')"
+                :testObject="item.test"
                 :details="item.oqlToJson.details"
+                @show-details="showTestDetails"
             />
           </template>
           <template v-slot:item.jsonToOql="{ item }">
             <test-result-cell
                 :value="item.jsonToOql.isPassing"
                 :loading="isLoadingCell(item.id, 'queryToOql')"
+                :testObject="item.test"
                 :details="item.jsonToOql.details"
+                @show-details="showTestDetails"
             />
           </template>
           <template v-slot:item.jsonToSearch="{ item }">
             <test-result-cell
                 :value="item.jsonToSearch.isPassing"
                 :loading="isLoadingCell(item.id, 'queryToSearch')"
+                :testObject="item.test"
                 :details="item.jsonToSearch.details"
+                @show-details="showTestDetails"
             />
           </template>
         </v-data-table>
       </v-card-text>
     </v-card>
+
+    <v-dialog v-model="showJsonDialog" max-width="800px">
+      <v-card>
+        <v-card-title>
+          Full Test JSON
+          <v-spacer></v-spacer>
+          <v-btn icon @click="showJsonDialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-container style="max-height: 600px; overflow-y: auto;">
+            <pre style="white-space: pre-wrap; word-wrap: break-word;">{{
+                JSON.stringify(selectedTestJson, null, 2)
+              }}</pre>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
 
     <v-dialog v-model="dialog" max-width="800px">
       <v-card>
@@ -132,17 +165,39 @@
       </v-card>
     </v-dialog>
 
-    <v-menu v-model="showPopover" :close-on-content-click="false"
-            :nudge-width="200" offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <span v-bind="attrs" v-on="on" style="display: none;"></span>
-      </template>
+    <v-dialog v-model="showDetailsDialog" max-width="90%">
       <v-card>
+        <v-card-title class="headline">
+          Test Details
+          <v-spacer></v-spacer>
+          <v-btn icon @click="showDetailsDialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
         <v-card-text>
-          <pre>{{ popoverContent }}</pre>
+          <v-row>
+            <v-col cols="6">
+              <h3>Test Details</h3>
+              <v-card outlined height="500px" class="overflow-y-auto">
+                <v-card-text>
+                  <pre>{{ JSON.stringify(selectedTestDetails, null, 2) }}</pre>
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col cols="6">
+              <h3>Full Test Object</h3>
+              <v-card outlined height="500px" class="overflow-y-auto">
+                <v-card-text>
+                  <pre>{{
+                      JSON.stringify(selectedFullTestObject, null, 2)
+                    }}</pre>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-card-text>
       </v-card>
-    </v-menu>
+    </v-dialog>
 
   </v-container>
 </template>
@@ -156,113 +211,152 @@ export default {
   name: "OQOTests",
   components: {
     TestResultCell: {
-      components: {VProgressCircular},
       props: ['value', 'loading', 'details'],
-      data() {
-        return {
-          showPopover: false
-        };
-      },
-      computed: {
-        color() {
-          return this.value === true ? 'green' : this.value === false ? 'red' : 'grey';
-        },
-        icon() {
-          return this.value === true ? '✓' : this.value === false ? '✗' : '−';
-        }
-      },
-      methods: {
-        showDetails() {
-          if (this.value === false && this.details) {
-            this.$root.$emit('show-popover', JSON.stringify(this.details, null, 2));
-          }
-        }
-      },
       render(h) {
         if (this.loading) {
-          return h(VProgressCircular, {
-            props: {
-              indeterminate: true,
-              size: 20,
-              width: 2
+          return h('div', {
+            style: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
             }
-          });
+          }, [
+            h(VProgressCircular, {
+              props: {
+                indeterminate: true,
+                size: 20,
+                width: 2
+              }
+            })
+          ]);
         }
-        return h('span', {
-          style: {
-            color: this.color,
-            fontWeight: 'bold',
-            cursor: this.value === false ? 'pointer' : 'default',
-            textDecoration: this.value === false ? 'underline' : 'none'
-          },
+
+        const cellStyle = {
+          backgroundColor: this.value === true ? '#4CAF50' : // Success color
+              this.value === false ? '#F44336' : // Error color
+                  'transparent', // Default
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        };
+
+        const contentStyle = {
+          color: this.value === true || this.value === false ? 'white' : 'grey',
+          fontWeight: 'bold',
+          textDecoration: this.value === false ? 'underline' : 'none',
+          cursor: this.value === false ? 'pointer' : 'default'
+        };
+
+        return h('div', {
+          style: cellStyle,
           on: {
-            click: this.showDetails
+            click: () => {
+              if (this.value === false && this.details) {
+                this.$emit('show-details', {
+                  details: Object.fromEntries(
+                      Object.entries(this.details).filter(([key]) => key !== "test")),
+                  testObject: this.details.test
+                });
+              }
+            }
           }
-        }, [this.icon]);
+        }, [
+          h('span', {style: contentStyle},
+              this.value === true ? 'PASS' :
+                  this.value === false ? 'FAIL' : '−'
+          )
+        ]);
       }
     },
     NatLangToJsonCell: {
-      components: {VProgressCircular},
       props: ['value', 'loading'],
-      data() {
-        return {
-          showPopover: false
-        };
-      },
-      methods: {
-        showDetails(subTest) {
-          if (!subTest.isPassing && subTest.details) {
-            this.$root.$emit('show-popover', JSON.stringify(subTest.details, null, 2));
-          }
-        }
-      },
       render(h) {
         if (this.loading) {
-          return h(VProgressCircular, {
-            props: {
-              indeterminate: true,
-              size: 20,
-              width: 2
-            }
-          });
-        }
-        if (!this.value || this.value.length === 0) {
-          return h('span', {
+          return h('div', {
             style: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+            }
+          }, [
+            h(VProgressCircular, {
+              props: {
+                indeterminate: true,
+                size: 20,
+                width: 2
+              }
+            })
+          ]);
+        }
+
+        if (!this.value || this.value.length === 0) {
+          return h('div', {
+            style: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
               color: 'grey',
               fontWeight: 'bold'
             }
           }, '−');
         }
-        return h('div', this.value.map((subTest, index) =>
-            h('div', {
-              key: index,
-              style: {
-                display: 'flex',
-                alignItems: 'center',
-                marginTop: '5px'
-              }
-            }, [
-              h('span', {
-                style: {
-                  color: subTest.isPassing ? 'green' : 'red',
-                  marginRight: '5px',
-                  cursor: !subTest.isPassing ? 'pointer' : 'default',
-                  textDecoration: !subTest.isPassing ? 'underline' : 'none'
-                },
-                on: {
-                  click: () => this.showDetails(subTest)
+
+        return h('div', {
+          style: {
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+          }
+        }, this.value.map((subTest, index) => {
+          const cellStyle = {
+            backgroundColor: subTest.isPassing ? '#4CAF50' : '#F44336',
+            cursor: !subTest.isPassing ? 'pointer' : 'default',
+            display: 'flex',
+            alignItems: 'center',
+            flex: 1,
+          };
+
+          const contentStyle = {
+            color: 'white',
+            fontWeight: 'bold',
+            marginRight: '8px',
+            textDecoration: !subTest.isPassing ? 'underline' : 'none',
+          };
+
+          return h('div', {
+            key: index,
+            style: cellStyle,
+            on: {
+              click: () => {
+                if (!subTest.isPassing && subTest.details) {
+                  this.$emit('show-details', {
+                    details: Object.fromEntries(
+                        Object.entries(subTest.details).filter(([key]) => key !== "test")),
+                    testObject: subTest.details.test
+                  });
                 }
-              }, [subTest.isPassing ? '✓' : '✗']),
-              h('span', subTest.prompt)
-            ])
-        ));
+              }
+            }
+          }, [
+            h('span', {style: contentStyle}, subTest.isPassing ? 'PASS' : 'FAIL'),
+            h('span', {style: {color: 'white'}}, subTest.prompt)
+          ]);
+        }));
       }
-    },
+    }
   },
   data() {
     return {
       passingPercentage: '-',
+      showDetailsDialog: false,
+      selectedTestDetails: null,
+      selectedFullTestObject: null,
+      showJsonDialog: false,
+      selectedTestJson: null,
       tests: [],
       selected: [],
       selectedTags: [],
@@ -329,6 +423,11 @@ export default {
           jsonToSearch: {isPassing: null, details: null},
         }));
       });
+    },
+    showTestDetails(payload) {
+      this.selectedTestDetails = payload.details;
+      this.selectedFullTestObject = payload.testObject;
+      this.showDetailsDialog = true;
     },
     calculatePassingPercentage() {
       let totalTests = 0;
@@ -440,5 +539,13 @@ export default {
 <style>
 .v-data-table > .v-data-table__wrapper > table > tbody > tr > td {
   white-space: normal !important;
+
+}
+</style>
+<style scoped>
+.v-btn.pa-0 {
+  min-width: 0;
+  width: 100%;
+  justify-content: flex-start;
 }
 </style>
