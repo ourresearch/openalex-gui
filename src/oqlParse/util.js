4c8d18dcd6afa50c584690c81a2599cd1ad279ae
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-29 23:26:17 -0500

    Base58 UUID helpers

--- src/oqlParse/util.js
+++ src/oqlParse/util.js
@@ -1,5 +1,7 @@
 import CryptoJS from "crypto-js";
 
+const BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+
 function invertMap(map) {
     const invertedMap = {};
     for (const key in map) {
@@ -15,7 +17,31 @@ function objectMD5(obj) {
     return CryptoJS.MD5(jsonString).toString();
 }
 
+function hexToBigInt(hex) {
+    return BigInt('0x' + hex);
+}
+
+function bigIntToBase58(num) {
+    let base58 = '';
+    const base = BigInt(58);
+    while (num > 0) {
+        const remainder = num % base;
+        base58 = BASE58_ALPHABET[remainder] + base58;
+        num = num / base;
+    }
+    return base58;
+}
+
+function objectMD5ShortUUID(obj) {
+    const md5Hash = objectMD5(obj);
+    const bigIntValue = hexToBigInt(md5Hash);
+    const base58Str = bigIntToBase58(bigIntValue);
+    return base58Str.slice(0, 5);
+}
+
+
+
 export {
     invertMap,
-    objectMD5,
+    objectMD5ShortUUID,
 };
