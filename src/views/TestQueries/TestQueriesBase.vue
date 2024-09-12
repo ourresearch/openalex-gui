61733d500708ef0e0932124a7ff8947e81adb926
Jason Priem <jason@ourresearch.org>
2024-09-12 15:16:24 +0100

    save point.

--- src/views/TestQueries/TestQueriesBase.vue
+++ src/views/TestQueries/TestQueriesBase.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="$store.state.testQueries.length">
+  <div>
     <v-container class="mt-6" >
       <div>
         <v-breadcrumbs
@@ -14,51 +14,7 @@
       </div>
       <div class="text-h4 mb-5 mt-3">{{ pageTitle }}</div>
     </v-container>
-    <v-container v-if="pageType === 'queries'">
-      <v-row dense>
-        <v-col
-            cols="12"
-            sm="6"
-            md="4"
-            lg="3"
-            v-for="(query, index) in $store.state.testQueries"
-            :key="index"
-        >
-          <test-query :config="query"/>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container v-else-if="pageType === 'query'">
-      <test-query
-          :config="$store.state.testQueries.find(q => q.id == $route.params.queryId)"
-      />
-    </v-container>
-    <v-container v-else-if="pageType === 'test-type'">
-      <v-card flat rounded>
-        <v-card-text>There's no page for Test Type right now.</v-card-text>
-      </v-card>
-    </v-container>
-    <template dense v-else-if="pageType === 'test'">
-      <test-query-oql
-          v-if="$route.params.testType === 'oql'"
-
-
-          :input="myTest.input"
-          :expected-response="myTest.expectedResponse"
-
-          :query-id="Number($route.params.queryId)"
-          :test-id="$route.params.testId"
-      />
-      <test-query-nat-lang
-          v-if="$route.params.testType === 'natlang'"
-
-          :input="myTest.input"
-          :expected-response="myTest.expectedResponse"
-
-          :query-id="Number($route.params.queryId)"
-          :test-id="Number($route.params.testId)"
-      />
-    </template>
+    <router-view />
 
 
   </div>
@@ -68,27 +24,15 @@
 <script>
 
 import {mapActions, mapGetters, mapMutations} from "vuex";
-import axios from "axios";
-import TestQuery from "@/components/TestQuery/TestQuery.vue";
-import TestQueryOql from "@/components/TestQuery/TestQueryOql.vue";
-import TestQueryNatLang from "@/components/TestQuery/TestQueryNatLang.vue";
 
 export default {
   name: "Template",
   components: {
-    TestQuery,
-    TestQueryNatLang,
-    TestQueryOql,
   },
   props: {},
   data() {
     return {
-      passCount: 0,
-      failCount: 0,
-
       foo: 42,
-      queries: [],
-      isLoading: false,
     }
   },
   computed: {
@@ -99,25 +43,26 @@ export default {
     ...mapGetters("search", [
       "query",
     ]),
-    completeCount(){
-      return this.failCount + this.passCount
-    },
-    testsCount(){
-      return this.query.length
-    },
-    loadingCount(){
-      return this.testsCount - this.completeCount
-    },
     breadcrumbItems() {
+      // /tests/:testSuiteId/:queryId/:testType/:testId
+
       const items = [
-        {text: 'Queries', to: '/test-queries', exact: true,}
+        {text: 'Tests', to: '/tests', exact: true,}
       ];
+
+      if (this.$route.params.testSuiteId) {
+        items.push({
+          text: `Suite ${this.$route.params.testSuiteId}`,
+          exact: true,
+          to: `/tests/${this.$route.params.testSuiteId}`
+        });
+      }
 
       if (this.$route.params.queryId) {
         items.push({
           text: `Query #${this.$route.params.queryId}`,
           exact: true,
-          to: `/test-queries/${this.$route.params.queryId}`
+          to: `/tests/${this.$route.params.testSuiteId}/${this.$route.params.queryId}`
         });
       }
 
@@ -125,7 +70,7 @@ export default {
         items.push({
           text: `${this.$route.params.testType}`,
           exact: true,
-          to: `/test-queries/${this.$route.params.queryId}/${this.$route.params.testType}`
+          to: `/tests/${this.$route.params.testSuiteId}/${this.$route.params.queryId}/${this.$route.params.testType}`
         });
       }
 
@@ -133,7 +78,7 @@ export default {
         items.push({
           text: `${this.$route.params.testId}`,
           exact: true,
-          to: `/test-queries/${this.$route.params.queryId}/${this.$route.params.testType}/${this.$route.params.testId}`
+          to: `/tests/${this.$route.params.testSuiteId}/${this.$route.params.queryId}/${this.$route.params.testType}/${this.$route.params.testId}`
         });
       }
       return items
@@ -160,47 +105,6 @@ export default {
       }[this.pageType]
     },
 
-
-    myQuery() {
-      if (!this.$route.params.queryId) {
-        return null
-      }
-      return this.$store.state.testQueries.find(q => q.id == this.$route.params.queryId)
-    },
-    myTestType() {
-      if (!this.$route.params.testType) {
-        return null
-      }
-      return this.$route.params.testType
-    },
-
-    myTest() {
-      if (!this.myQuery && this.myTestType && this.$route.params.testId) {
-        return null
-      }
-      if (this.myTestType === 'oql') {
-        const oqlTestsDict = {
-          "from-query": {
-            input: this.myQuery.query,
-            expectedResponse: this.myQuery.oql,
-          },
-          "to-query": {
-            input: this.myQuery.oql,
-            expectedResponse: this.myQuery.query,
-          }
-        }
-        return oqlTestsDict[this.$route.params.testId]
-
-      } else if (this.myTestType === 'natlang') {
-        return {
-          input: this.myQuery.natLang[this.$route.params.testId],
-          expectedResponse: this.myQuery.query,
-        }
-      } else {
-        throw new Error(`Unknown test type: ${this.myTestType}`)
-      }
-    }
-
   },
 
   methods: {
@@ -208,7 +112,6 @@ export default {
       "snackbar",
     ]),
     ...mapActions([
-      "getTestQueries",
     ]),
     ...mapMutations("search", []),
     ...mapActions("search", []),
@@ -219,7 +122,6 @@ export default {
   created() {
   },
   mounted() {
-    this.getTestQueries()
   },
   watch: {}
 }
