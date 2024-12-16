d4bad0a93354008db3d60fbc1aec2459df600539
Brett Lockspeiser <blocks@gmail.com>
2024-12-16 13:48:51 -0800

    Show error messages from backend

---
+++ src/components/Results/ResultsError.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="table-meta d-flex align-center pa-8"  style="height: 140px;">
+  <span style="">
+  <b>Search error:</b> {{$store.state.search.backend_error}}
+  </span>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "ResultsError",
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
