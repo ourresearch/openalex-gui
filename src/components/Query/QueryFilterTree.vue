39c8521adf7d678f30398675708f09f467e74559
Brett Lockspeiser <blocks@gmail.com>
2024-12-20 17:00:33 -0800

    Starting a UI for nested conjunction/disjunction queries

--- src/components/Query/QueryFilterTree.vue
+++ src/components/Query/QueryFilterTree.vue
@@ -10,21 +10,8 @@
         dense
     >
       <template v-slot:prepend="{item, open}">
-        <span class="">
-          <span class="number grey--text">
-            {{ item.siblingIndex + 1 }}.
-          </span>
-          <span
-              class="d-inline-flex justify-center"
-              style="min-width: 1.6em;"
-          >
-            <template v-if="item.siblingIndex === 0">
-              The
-            </template>
-            <template v-else>
-              {{ item.parentOperator }}
-            </template>
-          </span>
+        <span class="number grey--text" :style="{'margin-left': item.depth ? (item.depth * 20) + 'px' : 0 }">
+          {{ item.displayPath.join(".") }}.
         </span>
       </template>
 
@@ -32,8 +19,14 @@
         <query-filter-tree-leaf
             :column_id="item.column_id"
             :operator="item.operator"
+            :join-operator="item.joinOperator"
             :value="item.value"
+            :depth="item.depth"
             :subject-entity="subjectEntity"
+            :sibling-index="item.siblingIndex"
+            :can-group-above="item.canGroupAbove"
+            @setDepth="(depth) => setFilterDepth(item.path, depth)"
+            @setJoinOperator="(joinOperator) => setFilterJoinOperator(item.path, joinOperator)"
             @setOperator="(operator) => setFilterOperator(item.path, operator)"
             @setValue="(value) => setFilterValue(item.path, value)"
         />
@@ -41,11 +34,8 @@
 
       <template v-slot:append="{ item, open }">
         <div class="d-flex">
-          <v-btn
-              icon
-              @click="deleteFilter(item.path)"
-          >
-            <v-icon>mdi-delete-outline</v-icon>
+          <v-btn icon @click="deleteFilter(item.path)">
+            <v-icon>mdi-close</v-icon>
           </v-btn>
         </div>
       </template>
@@ -113,15 +103,16 @@ export default {
       "querySubjectEntityConfig",
     ]),
     displayFilters() {
-      return this.myFilters.map((f, i) => {
-        return {
+      const results = []
+      this.myFilters.forEach((f, i) => 
+        results.push({
           ...f,
           id: i, // for the treeview to know which nodes are open
           path: [i], // so we can find which nodes to update and delete
           siblingIndex: i, // just for display
-          parentOperator: "and", // just for display
-        }
-      })
+        })
+      )
+      return results
     },
     filtersToStore() {
       const ret = this.myFilters.filter(f => {
@@ -173,9 +164,43 @@ export default {
         // operator is not required, it'll use the default if not supplied later
         value: initValue,
       })
+      this.decorateMyFilters()
       if (columnType === "boolean") {
         this.applyFilters()
       }
+    },
+    decorateMyFilters() {
+      // Adds values to `myFilters` that requre looking at them as a set.
+      const results = []
+      this.myFilters.forEach((f, i) => {
+        f.depth = f.depth ?? 0
+        const prev = i > 0 ? results[i-1] : undefined
+        const canGroupAbove = prev && this.myFilters.length > 2 && prev.depth !== f.depth-1
+
+        // Add a display path repsenting depth / groups like [1], [2,3]
+        let displayPath = []
+        const prevDisplayPath = prev ? prev.displayPath : undefined
+        if (!prev) { 
+          displayPath = [1] 
+        } else if (prev.depth === f.depth-1) {
+          // Add a new level of depth
+          displayPath = [...prev.displayPath, 1] 
+        } else if (prev.depth > f.depth) {
+          // Removing levels of depth
+          displayPath = prev.displayPath.slice(0, -(prev.depth-f.depth))
+          displayPath[displayPath.length - 1] += 1
+        } else { 
+          // Adding to the exisiting level of depth
+          displayPath = [...prevDisplayPath.slice(0, -1), prevDisplayPath[prevDisplayPath.length - 1] + 1]
+        }
+
+        results.push({
+          ...f,
+          canGroupAbove,
+          displayPath,
+        })
+      })
+      this.myFilters = results
     },
     // read
     getFilterFromPath(path) {
@@ -183,19 +208,42 @@ export default {
       const index = path[0]
       return this.myFilters[index]
     },
-    // update
-    setFilterOperator(pathToFilter, operator) {
-      console.log("setFilterOperator", pathToFilter, operator)
-      const filterToUpdate = this.getFilterFromPath(pathToFilter)
-      Vue.set(filterToUpdate, "operator", operator)
-      this.applyFilters()
+    getFiltersInSameGroup(path) {
+      // Returns an array of all filters in the same join group as `path`
+      const testFilter = this.getFilterFromPath(path)
+      const testGroup = testFilter.displayPath.slice(0, -1).join(".")
+
+      return this.myFilters.filter(f => {
+        return testGroup === f.displayPath.slice(0, -1).join(".")
+      })
     },
+    // update
     setFilterValue(pathToFilter, value) {
       console.log("setFilterValue", pathToFilter, value)
       const filterToUpdate = this.getFilterFromPath(pathToFilter)
       Vue.set(filterToUpdate, "value", value)
       this.applyFilters()
     },
+    setFilterOperator(pathToFilter, operator) {
+      console.log("setFilterOperator", pathToFilter, operator)
+      const filterToUpdate = this.getFilterFromPath(pathToFilter)
+      Vue.set(filterToUpdate, "operator", operator)
+      this.applyFilters()
+    },
+    setFilterJoinOperator(pathToFilter, joinOperator) {
+      console.log("setFilterJoin", pathToFilter, joinOperator)
+      const filtersToUpdate = this.getFiltersInSameGroup(pathToFilter)
+      filtersToUpdate.forEach(f => {
+        Vue.set(f, "joinOperator", joinOperator)
+      })
+      this.applyFilters()
+    },
+    setFilterDepth(pathToFilter, depth) {
+      console.log("setFilterDepth", pathToFilter, depth)
+      const filterToUpdate = this.getFilterFromPath(pathToFilter)
+      Vue.set(filterToUpdate, "depth", depth)
+      this.applyFilters()
+    },          
     // delete
     deleteFilter(path) {
       // for now we just assume there is only a flat array of filters
@@ -221,7 +269,10 @@ export default {
     "filters": {
       handler: function (filters) {
         this.myFilters = _.cloneDeep(filters)
+        this.decorateMyFilters()
         this.openNodes = filters.map(f => f.id)
+        console.log("filters update")
+        console.log(this.myFilters)
       },
       immediate: true
     }
@@ -253,6 +304,9 @@ export default {
 }
 .query-filter-tree-button.inline-block {
   display: inline-block;
+}
+.v-treeview-node__prepend {
+  min-width: 0;
 }
 .v-treeview-node__root .v-treeview-node__level {
   width: 16px
