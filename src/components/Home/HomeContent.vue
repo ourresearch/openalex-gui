33f399f53a97a121517c2796e9a9379a2b8aa0d0
Brett Lockspeiser <blocks@gmail.com>
2024-12-31 10:16:07 -0800

    RA start page filter scrolling, view erroring query examples

--- src/components/Home/HomeContent.vue
+++ src/components/Home/HomeContent.vue
@@ -21,28 +21,29 @@
     </v-row>
     
     <v-row class="examples-header-line" />
-    <v-row class="examples-header text-subtitle-2">
+    <v-row class="examples-header text-h6">
       Example Questions
     </v-row>
-    <v-row class="example-filters">
+      <vue-horizontal  class="example-filters horizontal" :displacement="0.5" :button-between="false">
         <v-chip :outlined="typeFilter !== tag" v-for="tag in typeTags"
           @click="typeFilter = typeFilter === tag ? null : tag"
         >
           {{ tag }}
         </v-chip>
-        <br/>
         <v-chip :outlined="categoryFilter !== tag" v-for="tag in categoryTags"
           @click="categoryFilter = categoryFilter === tag ? null : tag"
         >
           {{ tag }}
         </v-chip>
-    </v-row>
+      </vue-horizontal>
+
     <v-row class="example-questions">
       <v-col cols="12" md="6" lg="4" v-for="query in showQueries" :key="query.question">
         <example-query
           :question="query.question"
           :type="query.type"
           :category="query.category"
+          :error="query.error"
           :url="query.url"
         />
       </v-col>
@@ -53,8 +54,67 @@
 
 <script>
 
+import VueHorizontal from 'vue-horizontal';
 import ExampleQuery from "@/components/Home/ExampleQuery.vue";
 
+export default {
+  name: "HomeContent",
+  components: {
+    VueHorizontal,
+    ExampleQuery,
+  },
+  props: {
+  },
+  data() {
+    return {
+      exampleQueries: exampleQueries,
+      typeFilter: null,
+      categoryFilter: null,
+      uiVariant: this.$store.state.uiVariant,
+    }
+  },
+  methods: {
+  },
+  computed: {
+    showQueries: function() {
+      const nExamples = 100
+      let examples = this.exampleQueries
+      if (this.uiVariant === "errors") {
+        return examples.filter(q => q.broken)
+      }
+      examples = examples.filter(q => !q.broken)
+      if (this.typeFilter) {
+        examples = examples.filter(q => q.type === this.typeFilter)
+      }
+      if (this.categoryFilter) {
+        examples = examples.filter(q => q.category === this.categoryFilter)
+      }
+      return examples.sort(() => Math.random() - 0.5).slice(0, nExamples);
+    },
+    typeTags: function() {
+      return [... new Set(exampleQueries.map(q => q.type))]
+    },
+    categoryTags: function() {
+      return [... new Set(exampleQueries.map(q => q.category))]
+    },
+  },
+  created() {
+  },
+  mounted() {
+  },
+  watch: {
+    typeFilter(newValue) {
+      if (newValue && this.categoryFilter) {
+        this.categoryFilter = null
+      }
+    },
+    categoryFilter(newValue) {
+      if (newValue && this.typeFilter) {
+        this.typeFilter = null
+      }
+    },
+  }
+}
 
 const exampleQueries = [
   {
@@ -245,47 +305,6 @@ const exampleQueries = [
   },
 ]
 
-export default {
-  name: "HomeContent",
-  components: {
-    ExampleQuery,
-  },
-  props: {
-  },
-  data() {
-    return {
-      exampleQueries: exampleQueries,
-      typeFilter: null,
-      categoryFilter: null,
-    }
-  },
-  methods: {
-  },
-  computed: {
-    showQueries: function() {
-      const nExamples = 100
-      let examples = this.exampleQueries
-      if (this.typeFilter) {
-        examples = examples.filter(q => q.type === this.typeFilter)
-      }
-      if (this.categoryFilter) {
-        examples = examples.filter(q => q.category === this.categoryFilter)
-      }
-      return examples.sort(() => Math.random() - 0.5).slice(0, nExamples);
-    },
-    typeTags: function() {
-      return [... new Set(exampleQueries.map(q => q.type))]
-    },
-    categoryTags: function() {
-      return [... new Set(exampleQueries.map(q => q.category))]
-    },
-  },
-  created() {
-  },
-  mounted() {
-  },
-  watch: {}
-}
 </script>
 
 <style lang="scss">
@@ -303,10 +322,25 @@ export default {
   border-bottom: 1px #ddd solid;
 }
 .row.examples-header {
-  margin: 10px 5px;
+  margin: 10px 14px;
 }
 .example-filters {
-  margin: 10px 1px -10px;
+  margin: 0px 10px 10px 10px;
+  height: 40px;
+  line-height: 40px;
+}
+.v-hl-btn-next, .v-hl-btn-prev {
+  top: -8px !important;
+  transform: scale(0.9);
+}
+.v-hl-btn-next {
+  right: -18px !important;
+}
+.v-hl-btn-prev {
+  left: -18px !important;
+}
+.v-hl-svg {
+  box-shadow: none !important;
 }
 .example-filters .v-chip {
   margin: 3px;
