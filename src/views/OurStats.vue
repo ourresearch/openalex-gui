d8dd56f0d95063b3baaed80180a1c90fcf3105d1
Brett Lockspeiser <blocks@gmail.com>
2025-01-13 10:38:27 -0800

    Don't show links to non-existant docs

--- src/views/OurStats.vue
+++ src/views/OurStats.vue
@@ -2,81 +2,79 @@
   <v-container class="page">
 
     <div class="text-h4">
-      Current stats
+      Current Stats
     </div>
-
-
 
     <div class="grey--text">Last updated {{ new Date().toDateString() }}</div>
     <v-divider class="my-3" />
     <v-row dense>
-          <v-col
-                  cols="12"
-                  lg="4"
-                  class=""
-                  v-for="cardData in cards"
-                  :key="cardData.name"
-          >
-            <v-hover>
-            <v-card
-                rounded
-                flat
-                class="fill-height d-flex flex-column pb-3"
-                :color="`${cardData.color} lighten-5`"
-            >
-<!--                :to="{name: 'Serp', params:{ entityType: cardData.name}}"-->
-              <div class="flex-grow-1 " :class="`${cardData.color}--text`">
-                <div class="d-flex align-baseline pa-4 pb-2">
-                  <v-icon left large :color="cardData.color">{{ cardData.icon }}</v-icon>
-                  <our-stats-entry
-                      :entity-type="cardData.name"
-                      class="text-h4 font-weight-bold"
+      <v-col
+        cols="12"
+        lg="4"
+        class=""
+        v-for="cardData in cards"
+        :key="cardData.name"
+      >
+        <v-hover>
+        <v-card
+            rounded
+            flat
+            class="fill-height d-flex flex-column pb-3"
+            :color="`${cardData.color} lighten-5`"
+        >
+          <div class="flex-grow-1 " :class="`${cardData.color}--text`">
+            <div class="d-flex align-baseline pa-4 pb-2">
+              <v-icon left large :color="cardData.color">{{ cardData.icon }}</v-icon>
+              <our-stats-entry
+                  :entity-type="cardData.name"
+                  class="text-h4 font-weight-bold"
 
-                  />
-                  <span class="ml-2 text-capitalize">
-                    {{ cardData.name }}
-                  </span>
-                  <v-spacer />
-                    <v-btn
-                        small
-                        icon
-                        :href="`https://docs.openalex.org/api-entities/${cardData.name}`"
-                        target="_blank"
-                    >
-                      <v-icon small>mdi-information-outline</v-icon>
-                    </v-btn>
+              />
+              <span class="ml-2 text-capitalize">
+                {{ cardData.name }}
+              </span>
+              <v-spacer />
+                <v-btn
+                    v-if="cardData.hasDocs"
+                    small
+                    icon
+                    :href="`https://docs.openalex.org/api-entities/${cardData.name}`"
+                    target="_blank"
+                >
+                  <v-icon small>mdi-information-outline</v-icon>
+                </v-btn>
 
-                </div>
-                <v-divider v-if="cardData.highlightFilters" />
-                <v-list class="pa-0" color="transparent" v-if="cardData.highlightFilters">
-                  <v-list-item
-                    v-for="highlightFilter in cardData.highlightFilters"
-                    :key="highlightFilter.key"
-                  >
-                    <v-list-item-content>
-                      <v-list-item-title>
-                          <our-stats-entry
-                            :entity-type="cardData.name"
-                            :loading-spinner-size="12"
-                            :filter-key="highlightFilter.key"
-                            :filter-value="highlightFilter.value"
-                            color="grey"
-                            class="font-weight-bold"
-                          />
-                          <span>
-                           {{ highlightFilter.displayName }}
-                          </span>
+            </div>
+            <v-divider v-if="cardData.highlightFilters" />
+            <v-list class="pa-0" color="transparent" v-if="cardData.highlightFilters">
+              <v-list-item
+                v-for="highlightFilter in cardData.highlightFilters"
+                :key="highlightFilter.key"
+              >
+                <v-list-item-content>
+                  <v-list-item-title>
+                      <our-stats-entry
+                        :entity-type="cardData.name"
+                        :loading-spinner-size="12"
+                        :filter-key="highlightFilter.key"
+                        :filter-value="highlightFilter.value"
+                        color="grey"
+                        class="font-weight-bold"
+                      />
+                      <span>
+                       {{ highlightFilter.displayName }}
+                      </span>
 
-                      </v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </div>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </div>
 
-            </v-card>
+        </v-card>
 
-            </v-hover>
-          </v-col>
+        </v-hover>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -89,35 +87,35 @@ import OurStatsEntry from "../components/OurStats/OurStatsEntry.vue";
 import {getFacetConfig} from "../facetConfigs";
 
 export default {
-  name: "Template",
+  name: "OurStats",
   components: {
     OurStatsEntry,
   },
   props: {},
   data() {
     return {
-      foo: 42,
       getFacetConfig,
     }
   },
   computed: {
-    ...mapGetters([
-
-    ]),
     cards(){
       const copy = _.cloneDeep(entityConfigs)
-      const list = Object.values(copy)
+      let list = Object.values(copy)
+
+      const entitiesWithDocs = ["works", "authors", "sources", "institutions", "topics", "keywords", "publishers", "funders", "concenpts"]
+
+      list = list.map(e => {
+        if (entitiesWithDocs.includes(e.name)) { e.hasDocs = true }
+        return e
+      })
+
       return list
     }
   },
-
   methods: {
     ...mapMutations([
       "snackbar",
     ]),
-    ...mapActions([]),
-
-
   },
   created() {
   },
