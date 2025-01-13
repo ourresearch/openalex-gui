6b36f68dafd5f5e9b0196711b013d7bbf9d045ab
Brett Lockspeiser <blocks@gmail.com>
2025-01-13 08:41:38 -0800

    Be sure to sort years as numbers not strings

--- src/components/GroupBy/GroupBy.vue
+++ src/components/GroupBy/GroupBy.vue
@@ -308,7 +308,7 @@ export default {
       )
       if (this.filterKey === "publication_year") {
         ret.sort((a, b) => {
-          return (a.value > b.value) ? -1 : 1
+          return (parseInt(a.value) > parseInt(b.value)) ? -1 : 1
         })
       }
       this.groups = ret
