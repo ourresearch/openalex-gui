cdca86480e00771441e903447152f0f8b9bbb1a5
Brett Lockspeiser <blocks@gmail.com>
2025-01-13 17:33:26 -0800

    OQL parsing/generation for disjunctions, testing updates

--- src/components/TestQuery/testQuery.js
+++ src/components/TestQuery/testQuery.js
@@ -24,6 +24,7 @@ const getTestQuery = async function (testSuiteId, queryId) {
 
 const localTestSuites = {
     disjunctions: [
+        // WORKS
         {
             title: "One work ID",
             oql: "get works where id is works/W1775749144;",
@@ -62,6 +63,7 @@ const localTestSuites = {
         },
         {
             title: "Two work IDs, and",
+            expectsZeroResults: true,
             oql: "get works where (id is works/W1775749144 and id is works/W2582743722);",
             query: {
                 get_rows: "works",
@@ -76,6 +78,126 @@ const localTestSuites = {
                             {
                                 column_id: "id",
                                 value: "works/W2582743722"
+                            }
+                        ]
+                    },
+                ],
+            }
+        },
+        {
+            title: "Intersecting work IDs, and/or+or",
+            oql: "get works where ((id is works/W1775749144 or id is works/W2582743722) and (id is works/W1775749144 or id is works/W2100837269));",
+            query: {
+                get_rows: "works",
+                filter_works: [
+                    {join: "and",
+                        filters: [
+                            {join: "or",
+                                filters: [
+                                    {column_id: "id",
+                                     value: "works/W1775749144"
+                                    },
+                                    {column_id: "id",
+                                     value: "works/W2582743722"
+                                    }
+                                ]
+                            },
+                            {join: "or",
+                                filters: [
+                                    {column_id: "id",
+                                     value: "works/W1775749144"
+                                    },
+                                    {column_id: "id",
+                                     value: "works/W2100837269"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                ],
+            }
+        },
+        {
+            title: "Works: letters, editorial, or erratum ",
+            expectsZeroResults: true,
+            oql: "get works where (type is types/letter or type is types/editorial or type is types/erratum);",
+            query: {
+                get_rows: "works",
+                filter_works: [
+                    {
+                        join: "or",
+                        filters: [
+                            {
+                                column_id: "type",
+                                value: "types/letter"
+                            },
+                            {
+                                column_id: "type",
+                                value: "types/editorial"
+                            },
+                            {
+                                column_id: "type",
+                                value: "types/erratum"
+                            },
+                        ]
+                    },
+                ],
+            }
+        },
+        // AUTHORS
+        {
+            title: "One author ID",
+            oql: "get authors where id is authors/A5066175077;",
+            query: {
+                get_rows: "authors",
+                filter_aggs: [                
+                    {
+                        column_id: "id",
+                        value: "authors/A5066175077"
+                    },
+                            
+                ]
+            }
+        },
+        {
+            title: "Two author IDs, or",
+            oql: "get authors where (id is authors/A5066175077 or id is authors/A5011180999);",
+            query: {
+                get_rows: "authors",
+                filter_aggs: [
+                    {
+                        join: "or",
+                        filters: [
+                            {
+                                column_id: "id",
+                                value: "authors/A5066175077"
+                            },
+                            {
+                                column_id: "id",
+                                value: "authors/A5011180999"
+                            }
+                        ]
+                    },
+                ],
+            }
+        },
+        {
+            title: "Two author IDs, and",
+            expectsZeroResults: true,
+            oql: "get authors where (id is authors/A5066175077 and id is authors/A5011180999);",
+            query: {
+                get_rows: "authors",
+                filter_aggs: [
+                    {
+                        join: "and",
+                        filters: [
+                            {
+                                column_id: "id",
+                                value: "authors/A5066175077"
+                            },
+                            {
+                                column_id: "id",
+                                value: "authors/A5011180999"
                             }
                         ]
                     },
