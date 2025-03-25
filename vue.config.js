22e79cc70e89c6048f481ff3b74da3632746e806
Brett Lockspeiser <blocks@gmail.com>
2025-03-25 09:12:21 -0700

    Use source maps for JS error tracking

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   lintOnSave: false,
-
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  productionSourceMap: true,
+    configureWebpack: {
+    devtool: process.env.NODE_ENV === 'production' 
+      ? 'source-map'  // High-quality source maps for production
+      : 'eval-cheap-module-source-map' // Faster source maps for development
+  }
 }
