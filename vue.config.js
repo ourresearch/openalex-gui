ca115253ddc0b775fe6db8bbbb8c240d76236818
Brett Lockspeiser <blocks@gmail.com>
2025-05-19 13:43:37 -0700

    Add webpack analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   lintOnSave: false,
   transpileDependencies: [
@@ -5,8 +7,9 @@ module.exports = {
   ],
   productionSourceMap: true,
   configureWebpack: {
+    //plugins: [new BundleAnalyzerPlugin()],
     devtool: process.env.NODE_ENV === 'production' 
       ? 'source-map'  // High-quality source maps for production
       : 'eval-cheap-module-source-map' // Faster source maps for development
-  },
+    },
 }
