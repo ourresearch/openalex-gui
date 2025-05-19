cbf75978b30afeca502bf80d1b90493782d1737e
Brett Lockspeiser <blocks@gmail.com>
2025-05-19 11:42:43 -0700

    Merge stores

--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,50 @@ Vue.use(Vuex);
 
 const stateDefaults = function () {
     const ret = {
-        q: "",
+        entityType: "works",
+        activeFilter: null,
+        activeFilterKey: null,
+        newFilterKey: null,
+        filterObjects: [],
+        appliedFilterObjects: [],
+        textSearch: "",
+        page: 1,
+        results: [],
+        resultsObject: null,
+        sort: null,
+        responseTime: null,
+        resultsCount: null,
         isLoading: false,
         snackbarIsOpen: false,
+        snackbarMsg: "",
+        snackbarIcon: null,
         snackbarColor: undefined,
         zoomId: null,
+        apiDialogIsOpen: true,
+        apiDialogUrl: "",
+        showFiltersDrawer: false,
+        facetZoom: null,
+        facetsListDialogIsOpen: false,
+        zoomIdsStack: [],
+        zoomDataResponses: [],
+        exportProgressUrl: null,
+        filterOptionChipOpenMenu: null,
+        isApiEditorShowing: false,
+        showColumns: [
+            "publication_year",
+            "type",
+            "open_access.is_oa",
+            "cited_by_count",
+        ],
+
+        serpTabs: [
+            {
+                id: null,
+                name: null,
+                query: undefined,
+            }
+        ],
+        isDevEnv: false,
         uiVariant: 'sentence-worksfirst', // for testing UI variations
         isInitialLoad: true, // used to for bypassing cache on freshloads
         testQueries: [],
@@ -28,6 +67,9 @@ export default new Vuex.Store({
         search,
     },
     mutations: {
+        setApiDialogUrl(state, url) {
+            state.apiDialogUrl = url;
+        },
         snackbar(state, arg) {
             state.snackbarIsOpen = true;
             if (typeof arg === "string") {
@@ -45,8 +87,14 @@ export default new Vuex.Store({
         setGlobalIsLoading(state, isLoading) {
             state.isLoading = !!isLoading;
         },
+        toggleFiltersDrawer(state) {
+            state.showFiltersDrawer = !state.showFiltersDrawer;
+        },
         setZoomId(state, id) {
             state.zoomId = id;
+        },
+        setEntityType(state, entityType) {
+            state.entityType = entityType;
         },
         setIsInitialLoad(state, isInitialLoad) {
             state.isInitialLoad = isInitialLoad;
@@ -58,8 +106,44 @@ export default new Vuex.Store({
     actions: {
     },
     getters: {
+        apiDialogUrl(state) {
+            return state.apiDialogUrl;
+        },
+        entityType(state) {
+            return state.entityType;
+        },
+        entityConfig(state) {
+            return entityConfigs[state.entityType];
+        },
+        showFiltersDrawer(state) {
+            return state.showFiltersDrawer;
+        },
+        facetZoom(state) {
+            return state.facetZoom;
+        },
         globalIsLoading(state) {
             return state.isLoading;
+        },
+        results(state) {
+            return state.results;
+        },
+        resultsCount(state) {
+            return state?.resultsObject?.meta?.count;
+        },
+        // adds stuff just used by the ui
+        textSearch(state, getters) {
+            return state.textSearch;
+        },
+        zoomId(state) {
+            return state.zoomIdsStack.slice(-1)[0]; // last item in stack
+        },
+        filtersZoom(state) {
+            return state.filtersZoom
+        },
+        facetsByCategory(state) {
+            return function (searchString) {
+                return facetsByCategory(state.entityType, searchString);
+            };
         },
         uiVariant(state) {
             return state.uiVariant;
