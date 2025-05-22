c728514b2a3456860b9e4f1bcfa81fa03012e21d
Brett Lockspeiser <blocks@gmail.com>
2025-05-22 13:17:34 -0700

    Move and ignore cypress examples

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'v2z8o5',
   e2e: {
     baseUrl: 'http://localhost:8080',
     experimentalStudio: true,
     viewportWidth: 1400,
     viewportHeight: 1000,
+    excludeSpecPattern: ['**/examples/**'],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -3,7 +3,8 @@ describe('OpenAlex App Loads', () => {
     cy.visit('/');
     cy.contains('OpenAlex');
   });
-  it('Visits the search page', () => {
+  
+  it('Search page loads', () => {
     cy.visit('/works?page=1&filter=default.search%3Akyle%20demes');
     cy.contains('kyle demes');
     cy.contains('Works');    
@@ -11,7 +12,7 @@ describe('OpenAlex App Loads', () => {
   });
 
   /* ==== Test Created with Cypress Studio ==== */
-  it('Visits Entity Drawer on SERP', function() {
+  it('Entity Drawer loads on Serp', function() {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('/works?page=1&filter=default.search%3Akyle%20demes&zoom=w2103321907');
     cy.get('.mt-4 > .v-toolbar__content > div > .theme--light').should('be.visible');
