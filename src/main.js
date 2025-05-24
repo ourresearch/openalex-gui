50500b0b10be44aab395437e54b1b42715020eaf
Brett Lockspeiser <blocks@gmail.com>
2025-05-23 17:42:58 -0700

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,80 +1,74 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import _ from 'lodash'
 import VueMeta from "vue-meta";
 import VScrollLock from "v-scroll-lock";
 import millify from "millify";
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
 
 import { navigation } from './navigation';
 import {url} from "./url"
 import tracking from "./tracking";
 import {prettyTitle, toPrecision, entityTypeFromId, shortenOpenAlexId} from "./util";
 import {createSimpleFilter} from "./filterConfigs";
-import _ from 'lodash'
 
-// import VueGtag from "vue-gtag";
+import App from './App.vue'
+
 
 tracking.setupJavaScriptErrorTracking();
 
-Vue.config.productionTip = false
-Vue.prototype.$prettyTitle = prettyTitle
+Vue.config.productionTip = false;
+Vue.prototype.$prettyTitle = prettyTitle;
 
 // we have to globally register this or it throws errors.
 // https://stackoverflow.com/a/58875919
 Vue.use(VueMeta, {
     refreshOnceOnNavigation: true
-})
-Vue.use(VScrollLock)
+});;
+Vue.use(VScrollLock);
 
-const VueTruncate = require('vue-truncate-filter')
-Vue.use(VueTruncate)
+const VueTruncate = require('vue-truncate-filter');
+Vue.use(VueTruncate);
 
-import FlagIcon from 'vue-flag-icon'
-
+import FlagIcon from 'vue-flag-icon';
 Vue.use(FlagIcon);
 
-import VuePluralize from 'vue-pluralize'
-
+import VuePluralize from 'vue-pluralize';
 Vue.use(VuePluralize)
 
-import VueShortkey from 'vue-shortkey'
+import VueShortkey from 'vue-shortkey';
+Vue.use(VueShortkey);
 
-Vue.use(VueShortkey)
-
-import AsyncComputed from 'vue-async-computed'
+import AsyncComputed from 'vue-async-computed';
 import {entityConfigs, urlPartsFromId} from "@/entityConfigs";
 
-Vue.use(AsyncComputed)
+Vue.use(AsyncComputed);
 
 
 Vue.filter("entityWorksLink", function (id) {
-    const entityType = entityTypeFromId(id)
-    if (!id || !entityType) return
-    const idForFilter = shortenOpenAlexId(id)
-
+    const entityType = entityTypeFromId(id);
+    if (!id || !entityType) { return; }
+    const idForFilter = shortenOpenAlexId(id);
 
     const filter = createSimpleFilter(
         "works",
         entityConfigs[entityType].filterKey,
         idForFilter,
-    )
+    );
     return {
         name: "Serp",
         params: {entityType: "works"},
         query: {filter: filter.asStr},
     }
-
-})
-
+});
 
 Vue.filter("entityZoomLink", function (id) {
-    if (!id) return
-    const shortId = shortenOpenAlexId(id)
-    const idEntityType = entityTypeFromId(id)
-    const newQuery = url.addToQuery(router.currentRoute.query, "zoom", shortId)
-    const params = {...router.currentRoute.params}
+    if (!id) { return; }
+    const shortId = shortenOpenAlexId(id);
+    const idEntityType = entityTypeFromId(id);
+    const newQuery = url.addToQuery(router.currentRoute.query, "zoom", shortId);
+    const params = {...router.currentRoute.params};
     // console.log("entityZoomLink", params)
 
     if (router.currentRoute.name === "Serp" && idEntityType === "works") {
@@ -82,42 +76,40 @@ Vue.filter("entityZoomLink", function (id) {
             name: "Serp",
             params,
             query: newQuery,
-        }
-    }
-    else {
+        };
+    } else {
         return {
-        name: "EntityPage",
-        params: urlPartsFromId(id)
-    }
+            name: "EntityPage",
+            params: urlPartsFromId(id)
+        };
     }
 });
 
 
 Vue.filter("zoomLink", function (fullId) {
-    if (!fullId) return
+    if (!fullId) { return; }
 
-    const zoomIds = router.currentRoute.query.zoom?.split(",") ?? []
-    zoomIds.push(shortId)
+    const zoomIds = router.currentRoute.query.zoom?.split(",") ?? [];
+    zoomIds.push(shortId);
 
-    const newQuery = url.addToQuery(router.currentRoute.query, "zoom", zoomIds.join())
+    const newQuery = url.addToQuery(router.currentRoute.query, "zoom", zoomIds.join());
 
     return {
         name: "Serp",
         query: newQuery,
-    }
-
+    };
 });
 
 
 Vue.filter("toPrecision", function (number, precision = 4) {
-    return toPrecision(number, precision)
+    return toPrecision(number, precision);
 });
 
 
 Vue.filter("capitalize", function (str) {
-    if (typeof str !== "string") return str
-    const firstLetter = str[0]
-    return firstLetter.toUpperCase() + str.substring(1)
+    if (typeof str !== "string") { return str; }
+    const firstLetter = str[0];
+    return firstLetter.toUpperCase() + str.substring(1);
 });
 
 
@@ -127,10 +119,10 @@ String.prototype.titleCase = function () {
   const stopWords = [
     "a", "an", "and", "as", "at", "but", "by", "for", "in", 
     "nor", "of", "on", "or", "so", "the", "to", "up", "yet"
-  ]
+  ];
 
   const fixedWords = ["OpenAlex", "ID", "IDs", "ROR", "ORCID", "DOI", "OA", "ISSN", "ISSNs", 
-    "DOAJ", "SDG", "SDGs", "FWCI", "URL", "CrossRef"]
+    "DOAJ", "SDG", "SDGs", "FWCI", "URL", "CrossRef"];
   
   // Create a mapping of lowercase fixed words to their correct form
   const fixedWordsMap = fixedWords.reduce((map, word) => {
@@ -165,21 +157,21 @@ Vue.filter("prettyName", function (name) {
     let ret = name
         .replace("ieee", "IEEE")
         .replace("United States of America", "United States")
-        .replace("United Kingdom of Great Britain and Northern Ireland", "United Kingdom")
+        .replace("United Kingdom of Great Britain and Northern Ireland", "United Kingdom");
 
     // make "journal-article" into Journal Article
-    const typeRe = /[a-z]+-[a-z]+/
-    if (typeRe.test(ret)) ret = ret.replace("-", " ")
+    const typeRe = /[a-z]+-[a-z]+/;
+    if (typeRe.test(ret)) { ret = ret.replace("-", " "); }
 
-    return ret
-})
+    return ret;
+});
 
 
 Vue.filter("idApiUrl", function (fullId) {
-    if (!fullId) return
-    const shortId = fullId.replace("https://openalex.org/", "")
-    return `https://api.openalex.org/${shortId}`
-})
+    if (!fullId) { return; }
+    const shortId = fullId.replace("https://openalex.org/", "");
+    return `https://api.openalex.org/${shortId}`;
+});
 
 
 Vue.filter("millify", function (number) {
@@ -189,11 +181,11 @@ Vue.filter("millify", function (number) {
         precision: 0,
             lowercase: false,
         }
-    )
-})
+    );
+});
 
 
-Vue.use(vuetify)
+Vue.use(vuetify);
 
 navigation.setRouter(router);
 
@@ -205,8 +197,9 @@ const vm = new Vue({
     data: {
         config: {}
     }
-})
+});
 
-window.vm = vm;
+// Expose the app to the window for debugging
+window.OpenAlex = vm;
 
-vm.$mount('#app')
+vm.$mount('#app');
