d6e2cfafb453ce8e1f97b260e974630306c0986f
Brett Lockspeiser <blocks@gmail.com>
2025-06-05 11:11:19 -0700

    Remove usage of v-list-item-action-text

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const webpack = require('webpack');
+
 module.exports = {
   lintOnSave: false,
   transpileDependencies: [
@@ -10,6 +12,13 @@ module.exports = {
     //plugins: [new BundleAnalyzerPlugin()],
     devtool: process.env.NODE_ENV === 'production' 
       ? 'source-map'  // High-quality source maps for production
-      : 'eval-cheap-module-source-map' // Faster source maps for development
-    },
+      : 'eval-cheap-module-source-map', // Faster source maps for development
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ]
+  },
 }
