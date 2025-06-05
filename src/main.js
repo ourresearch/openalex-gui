0a441ccc513b67dfd054d5cc2829b337799a05a8
Brett Lockspeiser <blocks@gmail.com>
2025-06-05 11:12:10 -0700

    restore vuetify style rules

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import 'vuetify/styles'
 import { createApp } from 'vue';
 import { createHead } from '@unhead/vue';
 import VScrollLock from 'v-scroll-lock';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import 'vuetify/styles'
 
 import { navigation } from './navigation';
 import tracking from './tracking';
