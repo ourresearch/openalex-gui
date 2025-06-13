5d8cdb54fecb978f67442f1139eed43f769c42f7
Brett Lockspeiser <blocks@gmail.com>
2025-06-13 09:48:14 -0700

    Results components v2->v3

--- src/components/Results/ResultsSearching.vue
+++ src/components/Results/ResultsSearching.vue
@@ -21,102 +21,104 @@
   </v-container>
 </template>
 
-<script>
-import _ from "lodash"
-import {mapGetters, mapMutations} from "vuex";
+<script setup>
+import { ref, computed, onMounted, onBeforeUnmount, watch } from 'vue';
+import { useStore } from 'vuex';
+import _ from 'lodash';
 
-export default {
-  name: "ResultsSearching",
-  components: {
-  },
-  data() {
-    return {
-      elapsedTime: 0, // in milliseconds
-      timer: null,
-      progressValue: 0,
-      messages: {
-        0: "We're crunching the numbers in real time for you.",
-        4: "Each query involves calculations on hundreds of millions of papers.",
-        10: "We do the math and aggregations so you don't have to.",
-        16: "The processing we're doing right now is not something you want to do yourself.",
-        22: "Have you tried opening a hundred million rows in Excel lately?",
-        30: "This is taking longer than usual, but we're still searching.",
-        40: "We're still searching. Your query has been reported to our developers for improvement."
-      }
-    }
-  },
-  computed: {
-    ...mapGetters("search", [
-      "isSearchCanceled",
-    ]),
-    currentMessage() {
-      const seconds = Math.floor(this.elapsedTime / 1000);
-      const timePoints = Object.keys(this.messages)
-        .map(Number)
-        .sort((a, b) => b - a);
-      
-      for (const time of timePoints) {
-        if (seconds >= time) {
-          return this.messages[time];
-        }
-      }
-      return null;
-    }
-  },
-  mounted() {
-    this.startTimer();
-  },
-  beforeUnmount() {
-    this.clearTimer();
-  },
-  methods: {
-    ...mapMutations("search", [
-      "setIsSearchCanceled"
-    ]),
-    cancelSearch() {
-      this.setIsSearchCanceled(true);
-      this.clearTimer();
-      this.resetTimer();
-    },
-    startTimer() {
-      this.timer = setInterval(() => {
-        this.elapsedTime += 50;
-        this.progressValue = (1 - 1 / Math.pow(2, this.elapsedTime / 10000)) * 90;
-      }, 50);
-    },
-    resetTimer() {
-      this.elapsedTime = 0;
-      this.progressValue = 0;
-    },
-    clearTimer() {
-      if (this.timer) {
-        clearInterval(this.timer);
-        this.timer = null;
-      }
-    },
-    restartTimer() {
-      this.clearTimer();
-      this.resetTimer();
-      this.startTimer();
-    },
-  },
-  watch: {
-    query(to, from) {
-      //console.log('Query changed', to, from);
-      //console.log("equal?", _.isEqual(to, from));
-      if (!_.isEqual(to, from)) {
-        this.restartTimer();
-      }
-    },
-    isSearchCanceled(to, from) {
-      if (from === true && to === false) {
-        this.restartTimer();
-      }
+defineOptions({ name: 'ResultsSearching' });
+
+const store = useStore();
+
+// State
+const elapsedTime = ref(0); // in milliseconds
+const timer = ref(null);
+const progressValue = ref(0);
+const messages = {
+  0: "We're crunching the numbers in real time for you.",
+  4: "Each query involves calculations on hundreds of millions of papers.",
+  10: "We do the math and aggregations so you don't have to.",
+  16: "The processing we're doing right now is not something you want to do yourself.",
+  22: "Have you tried opening a hundred million rows in Excel lately?",
+  30: "This is taking longer than usual, but we're still searching.",
+  40: "We're still searching. Your query has been reported to our developers for improvement."
+};
+
+// Computed properties
+const isSearchCanceled = computed(() => store.getters['search/isSearchCanceled']);
+const query = computed(() => store.getters['search/query']);
+
+const currentMessage = computed(() => {
+  const seconds = Math.floor(elapsedTime.value / 1000);
+  const timePoints = Object.keys(messages)
+    .map(Number)
+    .sort((a, b) => b - a);
+  
+  for (const time of timePoints) {
+    if (seconds >= time) {
+      return messages[time];
     }
   }
-}
+  return null;
+});
 
+// Methods
+const setIsSearchCanceled = (value) => store.commit('search/setIsSearchCanceled', value);
+
+const cancelSearch = () => {
+  setIsSearchCanceled(true);
+  clearTimer();
+  resetTimer();
+};
+
+const startTimer = () => {
+  timer.value = setInterval(() => {
+    elapsedTime.value += 50;
+    progressValue.value = (1 - 1 / Math.pow(2, elapsedTime.value / 10000)) * 90;
+  }, 50);
+};
+
+const resetTimer = () => {
+  elapsedTime.value = 0;
+  progressValue.value = 0;
+};
+
+const clearTimer = () => {
+  if (timer.value) {
+    clearInterval(timer.value);
+    timer.value = null;
+  }
+};
+
+const restartTimer = () => {
+  clearTimer();
+  resetTimer();
+  startTimer();
+};
+
+// Lifecycle hooks
+onMounted(() => {
+  startTimer();
+});
+
+onBeforeUnmount(() => {
+  clearTimer();
+});
+
+// Watchers
+watch(query, (to, from) => {
+  if (!_.isEqual(to, from)) {
+    restartTimer();
+  }
+});
+
+watch(isSearchCanceled, (to, from) => {
+  if (from === true && to === false) {
+    restartTimer();
+  }
+});
 </script>
+
 
 <style scoped lang="scss">
 * {
