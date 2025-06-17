3ea6ac2800d36759ce0551d39894941ad449d5b2
Brett Lockspeiser <blocks@gmail.com>
2025-06-17 12:32:46 -0700

    OurStats V2->V3

--- src/views/OurStats.vue
+++ src/views/OurStats.vue
@@ -1,126 +1,119 @@
 <template>
   <v-container class="page">
 
-    <div class="text-h4">
-      Data Stats
-    </div>
-    <div class="text-grey mb-2">
-      Last updated {{ new Date().toDateString() }}
-    </div>
+    <v-row>
+      <v-col cols="12">
+        <div class="text-h4">
+          Data Stats
+        </div>
+        <div class="text-grey mb-2">
+          Last updated {{ new Date().toDateString() }}
+        </div>
+      </v-col>
+    </v-row>
 
+    <v-row>
     <v-card rounded flat class="d-flex flex-wrap pa-4">
       <v-col
         cols="12"
         lg="4"
-        class=""
         v-for="cardData in cards"
         :key="cardData.name"
       >
         <v-hover>
-        <v-card
+          <v-card
             rounded
             flat
             class="fill-height d-flex flex-column pb-3"
-            :color="`${cardData.color} lighten-5`"
-        >
-          <div class="flex-grow-1 " :class="`${cardData.color}--text`">
-            <div class="d-flex align-baseline pa-4 pb-2">
-              <v-icon start size="large" :color="cardData.color">{{ cardData.icon }}</v-icon>
-              <our-stats-entry
+            :color="`${cardData.color}-lighten-4`"
+          >
+            <div class="flex-grow-1 " :class="`${cardData.color}--text`">
+              <div class="d-flex align-baseline pa-4 pb-2">
+                <v-icon start size="large" :color="cardData.color">{{ cardData.icon }}</v-icon>
+                <our-stats-entry
                   :entity-type="cardData.name"
                   class="text-h4 font-weight-bold"
-
-              />
-              <span class="ml-2 text-capitalize">
-                {{ cardData.name }}
-              </span>
-              <v-spacer />
-                <v-btn
+                />
+                <span class="ml-2 text-capitalize">
+                  {{ cardData.name }}
+                </span>
+                <v-spacer />
+                  <v-btn
                     v-if="cardData.hasDocs"
                     size="small"
+                    variant="plain"
                     icon
                     :href="`https://docs.openalex.org/api-entities/${cardData.name}`"
                     target="_blank"
+                  >
+                    <v-icon size="small">mdi-information-outline</v-icon>
+                  </v-btn>
+              </div>
+
+              <v-divider v-if="cardData.highlightFilters" />
+              
+              <v-list class="pa-0 highlight-filters" v-if="cardData.highlightFilters">
+                <v-list-item
+                  v-for="highlightFilter in cardData.highlightFilters"
+                  :key="highlightFilter.key"
                 >
-                  <v-icon size="small">mdi-information-outline</v-icon>
-                </v-btn>
-
-            </div>
-            <v-divider v-if="cardData.highlightFilters" />
-            <v-list class="pa-0" color="transparent" v-if="cardData.highlightFilters">
-              <v-list-item
-                v-for="highlightFilter in cardData.highlightFilters"
-                :key="highlightFilter.key"
-              >
-                
                   <v-list-item-title>
-                      <our-stats-entry
-                        :entity-type="cardData.name"
-                        :loading-spinner-size="12"
-                        :filter-key="highlightFilter.key"
-                        :filter-value="highlightFilter.value"
-                        color="grey"
-                        class="font-weight-bold"
-                      />
-                      <span>
-                       {{ highlightFilter.displayName }}
-                      </span>
-
+                    <our-stats-entry
+                      :entity-type="cardData.name"
+                      :loading-spinner-size="12"
+                      :filter-key="highlightFilter.key"
+                      :filter-value="highlightFilter.value"
+                      class="font-weight-bold"
+                    />
+                    <span class="ml-1">
+                      {{ highlightFilter.displayName }}
+                    </span>
                   </v-list-item-title>
-                
-              </v-list-item>
-            </v-list>
-          </div>
-
-        </v-card>
-
+                </v-list-item>
+              </v-list>
+            </div>
+          </v-card>
         </v-hover>
       </v-col>
     </v-card>
+    </v-row>
   </v-container>
 </template>
 
-<script>
 
-import _ from "lodash";
+<script setup>
+import _ from 'lodash';
 import { useHead } from '@unhead/vue';
+import { computed } from 'vue';
 
-import {entityConfigs} from "../entityConfigs";
-import OurStatsEntry from "../components/OurStats/OurStatsEntry.vue";
+import { entityConfigs } from '../entityConfigs';
+import OurStatsEntry from '../components/OurStats/OurStatsEntry.vue';
 
-export default {
-  name: "OurStats",
-  created() {
-    useHead({ title: 'Data Stats' });
-  },
-  components: {
-    OurStatsEntry,
-  },
-  props: {},
-  data() {},
-  computed: {
-    cards() {
-      const copy = _.cloneDeep(entityConfigs);
-      let list = Object.values(copy);
 
-      const entitiesWithDocs = ["works", "authors", "sources", "institutions", "topics", "keywords", "publishers", "funders", "concepts"]
+defineOptions({ name: 'OurStats' });
 
-      list = list.map((e, i) => {
-        if (entitiesWithDocs.includes(e.name)) { e.hasDocs = true; }
-        if (!e.color) {
-          const colors = ["blue", "green", "orange", "purple", "pink", "brown", "teal", "indigo", ]
-          e.color = colors[i % colors.length];
-        }
-        return e;
-      })
+useHead({ title: 'Data Stats' });
 
-      return list;
-    }
-  },
-}
+const entitiesWithDocs = [
+  'works', 'authors', 'sources', 'institutions',
+  'topics', 'keywords', 'publishers', 'funders', 'concepts'
+];
+
+const cards = computed(() => {
+  const copy = _.cloneDeep(entityConfigs);
+  const colors = ['blue', 'green', 'orange', 'purple', 'pink', 'teal', 'indigo'];
+
+  return Object.values(copy).map((e, i) => {
+    if (entitiesWithDocs.includes(e.name)) e.hasDocs = true;
+    if (!e.color) e.color = colors[i % colors.length];
+    return e;
+  });
+});
 </script>
 
 
 <style scoped lang="scss">
-
+.highlight-filters {
+  background-color: transparent !important;
+}
 </style>
