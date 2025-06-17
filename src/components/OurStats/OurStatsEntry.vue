7278b42cfd7baf6c9104cd22fcce20251ff16408
Brett Lockspeiser <blocks@gmail.com>
2025-06-17 12:32:46 -0700

    OurStats V2->V3

--- src/components/OurStats/OurStatsEntry.vue
+++ src/components/OurStats/OurStatsEntry.vue
@@ -1,61 +1,55 @@
 <template>
   <span>
-    <v-progress-circular :color="color" :size="loadingSpinnerSize" v-if="isLoading" indeterminate></v-progress-circular>
-    <span v-if="count">
-    {{ filters.millify(count) }}
-
+    <v-progress-circular
+      :color="props.color"
+      :size="props.loadingSpinnerSize"
+      v-if="isLoading"
+      indeterminate
+    />
+    <span v-else-if="count">
+      {{ filters.millify(count) }}
     </span>
   </span>
 </template>
 
 
-<script>
+<script setup>
+import { ref, watchEffect } from 'vue';
+import { api } from '@/api';
+import filters from '@/filters';
 
-import {api} from "@/api";
-import filters from "@/filters";
+defineOptions({ name: 'OurStatsEntry' });
 
-export default {
-  name: "OurStatsEntry",
-  components: {
+const props = defineProps({
+  entityType: String,
+  loadingSpinnerSize: {
+    type: Number,
+    default: 28,
   },
-  props: {
-    entityType: String,
-    loadingSpinnerSize: {
-      type: Number,
-      default: 28
-    },
-    filterKey: String,
-    filterValue: [String, Boolean],
-    color: String,
-  },
-  data() {
-    return {
-      isLoading: false,
-      filters,
-    }
-  },
-  computed: {
-  },
-  asyncComputed: {
-    async count(){
-      this.isLoading = true;
-      const myUrl = "/" + this.entityType;
-      const params = {};
-      if (this.filterKey && this.filterValue) {
-        params.filter = `${this.filterKey}:${this.filterValue}`;
-      }
+  filterKey: String,
+  filterValue: [String, Boolean],
+  color: String,
+});
 
-      const resp = await api.get(myUrl, params);
-      this.isLoading = false;
-      return resp.meta.count;
-    }
-  },
-  methods: {
+const count = ref(null);
+const isLoading = ref(false);
+
+watchEffect(async () => {
+  isLoading.value = true;
+  const myUrl = `/${props.entityType}`;
+  const params = {};
+  if (props.filterKey && props.filterValue !== undefined) {
+    params.filter = `${props.filterKey}:${props.filterValue}`;
   }
-}
+
+  try {
+    const resp = await api.get(myUrl, params);
+    count.value = resp.meta.count;
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
-
 <style scoped lang="scss">
-
 </style>
