d5564a97a27a303d9c73fcfb6568f984b75ff6e4
Brett Lockspeiser <blocks@gmail.com>
2025-06-24 11:17:29 -0700

    SearchFromText V2->V3

--- src/components/SearchFromText.vue
+++ src/components/SearchFromText.vue
@@ -1,24 +1,23 @@
 <template>
   <v-textarea
-      v-model="q"
-      :disabled="isNatLangLoading || disabled"
-      autofocus
-      auto-grow
-      variant="filled"
-      rounded
-      hide-details
-      rows="2"
-      :placeholder="placeholder"
-      @keydown.enter.exact.prevent="applyQ"
-      :class="{oql: selectedInputType === 'oql'}"
+    v-model="q"
+    :disabled="isNatLangLoading || disabled"
+    autofocus
+    auto-grow
+    variant="filled"
+    rounded
+    hide-details
+    rows="2"
+    :placeholder="placeholder"
+    @keydown.enter.exact.prevent="applyQ"
+    :class="{oql: selectedInputType === 'oql'}"
   >
-    <!--      prepend-inner-icon="mdi-code-parentheses-box"-->
     <template v-slot:append>
       <v-btn
-          size="large" icon style="margin-top: -11px; margin-right: -13px;"
-          @click="applyQ"
-          :disabled="isNatLangLoading"
-          :loading="isNatLangLoading"
+        size="large" icon style="margin-top: -11px; margin-right: -13px;"
+        @click="applyQ"
+        :disabled="isNatLangLoading"
+        :loading="isNatLangLoading"
       >
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
@@ -27,23 +26,22 @@
       <v-menu class="rounded-lg" max-width="300" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn
-              size="large"
-              v-bind="props"
-              style="margin: -11px 0 0 -19px; min-width: 1px; border-radius: 10px;"
-              class="pl-1 pr-0"
-              variant="text"
+            size="large"
+            v-bind="props"
+            style="margin: -11px 0 0 -19px; min-width: 1px; border-radius: 10px;"
+            class="pl-1 pr-0"
+            variant="text"
           >
             <v-icon>{{ inputTypes.find(it => it.id === selectedInputType).icon }}</v-icon>
-            <!--            {{ inputTypes.find(it => it.id === selectedInputType).displayName }}-->
             <v-icon>mdi-menu-down</v-icon>
           </v-btn>
         </template>
         <v-list>
           <v-list-item
-              v-for="inputType in inputTypes"
-              :key="inputType.id"
-              :active="selectedInputType === inputType.id"
-              @click="selectedInputType = inputType.id"
+            v-for="inputType in inputTypes"
+            :key="inputType.id"
+            :active="selectedInputType === inputType.id"
+            @click="selectedInputType = inputType.id"
           >
             <v-icon>{{ inputType.icon }}</v-icon>
             
@@ -60,98 +58,101 @@
   </v-textarea>
 </template>
 
+<script setup>
+import { ref, computed, watch } from 'vue';
+import { useStore } from 'vuex';
+import axios from 'axios';
 
-<script>
-import axios from "axios";
-import {mapActions, mapGetters} from "vuex";
-
-
-export default {
+defineOptions({
   name: 'SearchFromText',
-  props: {
-    disabled: Boolean,
-    selected: String,
-    naturalLanguage: Boolean,
+});
 
-  },
-  data() {
-    return {
-      q: "",
-      isNatLangLoading: false,
-      selectedInputType: "oql",
-      inputTypes: [
-        {
-          id: "natural-language",
-          displayName: "Natural language",
-          icon: "mdi-message-text",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        },
-        {
-          id: "oql",
-          displayName: "OQL",
-          icon: "mdi-code-parentheses-box",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        },
-      ]
-    }
-  },
-  computed: {
-    ...mapGetters("search", [
-    "queryOql"]),
-    placeholder() {
-      return this.selectedInputType === "natural-language" ?
-          "Enter natural language query" :
-          "Enter OQL query"
-    }
-  },
-  methods: {
-    ...mapActions("search", [
-      "createSearch",
-      "createSearchFromOql",
-      "createSearchFromQuery",
-    ]),
-    applyQ() {
-      return (this.selectedInputType === "natural-language") ?
-          this.applyNatLang() :
-          this.applyOql();
-    },
-    async applyNatLang() {
-      this.isNatLangLoading = true;
-      console.log("applyNatLang", this.q);
-      const myURl = `https://api.openalex.org/text/oql?natural_language=${this.q}`;
-      const resp = await axios.get(myURl);
-      this.isNatLangLoading = false;
+defineProps({
+  disabled: Boolean,
+  selected: String,
+  naturalLanguage: Boolean,
+});
 
-      this.createSearchFromQuery(resp.data);
-    },
-    applyOql() {
-      this.isNatLangLoading = true;
-      this.createSearchFromOql(this.q);
-      this.isNatLangLoading = false
-    }
-  },
-  mounted() {
-  },
-  watch: {
-    "queryOql": {
-      handler: function (newVal) {
-        if (this.selectedInputType === "oql") {
-          this.q = newVal
-        }
-      },
-      immediate: true
-    },
-    selectedInputType() {
-      if (this.selectedInputType === "natural-language") {
-        this.q = "";
-      } else {
-        this.q = this.queryOql;
-      }
-    }
+const store = useStore();
 
-  }
+// Local state
+const q = ref('');
+const isNatLangLoading = ref(false);
+const selectedInputType = ref('oql');
+
+const inputTypes = [
+  {
+    id: 'natural-language',
+    displayName: 'Natural language',
+    icon: 'mdi-message-text',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: 'oql',
+    displayName: 'OQL',
+    icon: 'mdi-code-parentheses-box',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
+
+// Vuex getters
+const queryOql = computed(() => store.getters['search/queryOql']);
+
+// Computed placeholder
+const placeholder = computed(() =>
+  selectedInputType.value === 'natural-language'
+    ? 'Enter natural language query'
+    : 'Enter OQL query'
+);
+
+// Vuex actions
+const createSearchFromOql = (oql) => store.dispatch('search/createSearchFromOql', oql);
+const createSearchFromQuery = (query) => store.dispatch('search/createSearchFromQuery', query);
+
+// Methods
+function applyQ() {
+  return selectedInputType.value === 'natural-language' ? applyNatLang() : applyOql();
 }
+
+async function applyNatLang() {
+  isNatLangLoading.value = true;
+  console.log('applyNatLang', q.value);
+  const myUrl = `https://api.openalex.org/text/oql?natural_language=${q.value}`;
+  try {
+    const resp = await axios.get(myUrl);
+    createSearchFromQuery(resp.data);
+  } catch (err) {
+    console.error('Error fetching natural language query:', err);
+  }
+  isNatLangLoading.value = false;
+}
+
+function applyOql() {
+  isNatLangLoading.value = true;
+  createSearchFromOql(q.value);
+  isNatLangLoading.value = false;
+}
+
+// Watchers
+watch(
+  () => queryOql.value,
+  (newVal) => {
+    if (selectedInputType.value === 'oql') {
+      q.value = newVal;
+    }
+  },
+  { immediate: true }
+);
+
+watch(
+  () => selectedInputType.value,
+  () => {
+    q.value = selectedInputType.value === 'natural-language' ? '' : queryOql.value;
+  }
+);
 </script>
+
+
 <style lang="scss">
 textarea {
   //padding-top: 5px !important;
