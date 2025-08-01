cfae3224839bd780fe47c88cdb1a86f86755e187
Brett Lockspeiser <blocks@gmail.com>
2025-08-01 13:07:03 -0700

    Journal Curation UI updates

--- src/views/JournalCuration.vue
+++ src/views/JournalCuration.vue
@@ -1,31 +1,30 @@
 <template>
   <div class="color-2 py-0 py-sm-12" style="min-height: 70vh;" ref="scrollContainer">
     <v-container fluid class="pa-0 pa-sm-4" style="max-width: 900px;">
-      <div class="text-h4 mb-2">
+      <div class="text-h3 mb-2">
         Unpaywall Journal Curation
       </div>
 
       <div class="text-subtitle-2 mb-3 text-grey-darken-1">
-        Sometimes Unpaywall makes errors. You can submit fixes to journals here. Corrections will show up in a few days.
+        Change the Open Access status of journals. Available to logged in librarians. Changes will show up in a few hours.
       </div>
 
       <v-text-field
         v-model="search"
-        variant="solo"
-        color="blue-lighten-2"
+        variant="solo-filled"
+        bg-color="#dbe2eb"
         flat
         clearable
         clear-icon="mdi-close"
         hide-details
-        rounded
+        rounded="pill"
         density="default"
         prepend-inner-icon="mdi-magnify"
         class="mb-3"
-        placeholder="Search by title or ISSN..."
+        placeholder="Search by title or ISSN"
       ></v-text-field>
 
-
-      <div class="mb-5 px-4">
+      <div class="mb-6 px-4">
       <!-- Open Access Filter -->
         <span class="text-grey-darken-1 mr-2" style="font-size: 12px;">Filters:</span>
 
@@ -33,13 +32,13 @@
           <template #activator="{ props }">
             <span class="mr-1">
               <template v-if="openAccessFilter === 'all'">
-                <v-btn v-bind="props" variant="flat" size="small" rounded class="text-grey-darken-1">
+                <v-btn v-bind="props" variant="flat" size="default" rounded class="text-grey-darken-1" style="border: 1px solid #DCE4ED;">
                   Open Access
                   <v-icon icon="mdi-menu-down" end class="mr-n1"></v-icon>
                 </v-btn>
               </template>
               <template v-else>
-                <v-btn v-bind="props" color="blue-darken-2" variant="tonal" size="small" rounded>
+                <v-btn v-bind="props" color="blue-darken-2" variant="tonal" size="default" rounded>
                   {{ openAccessFilter === 'open' ? 'Open' : 'Closed' }}
                   <v-icon icon="mdi-close" class="mr-n1" end @click.stop="openAccessFilter = 'all'"></v-icon>
                 </v-btn>
@@ -68,13 +67,13 @@
         <v-menu v-model="worksMenu" location="bottom start" :close-on-content-click="false">
           <template #activator="{ props }">
             <template v-if="worksFilter === 0">
-              <v-btn v-bind="props" variant="flat" size="small" rounded class="text-grey-darken-1">
+              <v-btn v-bind="props" variant="flat" size="default" rounded class="text-grey-darken-1" style="border: 1px solid #DCE4ED;">
                 Works
                 <v-icon icon="mdi-menu-down" end color="grey-darken-1" class="mr-n1"></v-icon>
               </v-btn>
             </template>
             <template v-else>
-              <v-btn v-bind="props" color="blue-darken-2" variant="tonal" size="small">
+              <v-btn v-bind="props" color="blue-darken-2" rounded variant="tonal" size="default">
                 &gt;{{ filters.millify(worksFilter) }} works
                 <v-icon icon="mdi-close" class="mr-n1" end @click.stop="worksFilter = 0"></v-icon>
               </v-btn>
@@ -108,7 +107,7 @@
       </div>
 
       <v-alert v-if="showSuccessMessage" type="success" density="compact">
-        Your correction has been received and will be reviewed within a few days.
+        Your correction has been received and will be processed within a few hours.
         <br />
         Thank you for your help.
       </v-alert>
@@ -116,54 +115,48 @@
         There was an error submitting your correction. Please try again later.
       </v-alert>
 
-
-      <v-card flat rounded="xl" class="pa-4">
-                  
+      <v-card flat rounded="xl" class="pa-4">   
         <div>
           <div v-if="searchResults.length > 0">
             <v-data-table
             :headers="headers"
             :items="searchResults"
             :items-per-page="100"
+            @click:row="onRowClick"
             hide-default-footer
-          >          
-            <template #item.display_name="{ value, item }">
-              <v-tooltip v-if="pendingCorrections.includes(item.issn_l)" location="bottom" text="A correction has been submitted for this journal. It will be reviewed within a few days.">
-                <template #activator="{ props }">
-                  <div v-bind="props">
-                    <v-icon icon="mdi-timer-sand" size="default" color="grey-lighten-1"></v-icon>
-                    {{ value }}
-                  </div>
+          >       
+            <template #item.is_oa="{ value, item }">
+              <div class="mr-n2">
+                <v-tooltip v-if="pendingCorrections.includes(item.issn_l)" location="bottom" text="A submitted change is currently pending for this journal. It will be processed within a few hours.">
+                  <template #activator="{ props }">
+                    <div v-bind="props">
+                      <v-icon icon="mdi-timer-sand" color="grey"></v-icon>
+                    </div>
+                  </template>
+                </v-tooltip>
+                <template v-else-if="value">
+                  <v-tooltip text="Open Access" location="bottom">
+                    <template #activator="{ props }">
+                      <v-icon icon="mdi-lock-open-variant" color="yellow-darken-2" v-bind="props"></v-icon>
+                    </template>
+                  </v-tooltip>
                 </template>
-              </v-tooltip>
-              <v-tooltip v-else-if="!item.issn_l" location="bottom" text="This journal record does not have an ISSN. We currently require an ISSN to submit corrections.">
-                <template #activator="{ props }">
-                  <div v-bind="props">
-                    {{ value }}
-                  </div>
+                <template v-else>
+                  <v-tooltip text="Closed Access" location="bottom">
+                    <template #activator="{ props }">
+                      <v-icon icon="mdi-lock" color="grey-lighten-1" v-bind="props"></v-icon>
+                    </template>
+                  </v-tooltip>
                 </template>
-              </v-tooltip>
-              <div v-else class="pr-2 cursor-pointer" @click="editJournal(item)">
-
-                <span class="journal-title">{{ value }}</span>
               </div>
             </template>
 
-            <template #item.is_oa="{ value }">
-              <template v-if="value">
-                <v-tooltip text="Open Access" location="bottom">
-                  <template #activator="{ props }">
-                    <v-icon icon="mdi-lock-open" color="yellow-darken-2" v-bind="props"></v-icon>
-                  </template>
-                </v-tooltip>
-              </template>
-              <template v-else>
-                <v-tooltip text="Closed Access" location="bottom">
-                  <template #activator="{ props }">
-                    <v-icon icon="mdi-lock" color="grey-lighten-1" v-bind="props"></v-icon>
-                  </template>
-                </v-tooltip>
-              </template>
+            <template #item.display_name="{ value, item }">
+
+              <div class="pr-2 py-1">
+                <div class="journal-title">{{ value }}</div>
+                <div class="journal-issn text-caption text-grey-darken-1">{{ item.issn_l }}</div>
+              </div>
             </template>
 
             <template #item.works_count="{ value }">
@@ -179,28 +172,35 @@
                 </template>
                 <v-card rounded="xl">
                   <v-list class="text-grey-darken-2" style="font-size: 14px;">
+                    <v-list-item @click="editJournal(item)">
+                      Change Open Access Status
+                    </v-list-item>
                     <v-list-item :href="item.id" target="_blank">
-                      <v-icon icon="mdi-open-in-new" size="x-small" color="grey"></v-icon>
                       OpenAlex profile
+                      <v-icon icon="mdi-open-in-new" size="x-small" color="grey"></v-icon>
                     </v-list-item>
                     <v-list-item :href="item.homepage_url" target="_blank">
-                      <v-icon icon="mdi-open-in-new" size="x-small" color="grey"></v-icon>
                       Journal website
+                      <v-icon icon="mdi-open-in-new" size="x-small" color="grey"></v-icon>
                     </v-list-item>
                   </v-list>
                 </v-card>
               </v-menu>
             </template>
-            </v-data-table>
-            <div v-if="searchResultsTotalCount > 100" class="text-center text-body-2 text-grey-darken-1 pt-8">
-              {{ searchResultsTotalCount - 100 }} additional journals match this search.
-            </div>
+          </v-data-table>
+
+          <div v-if="searchResultsTotalCount > 100" class="text-center text-body-2 text-grey-darken-1 pt-8">
+            <v-pagination
+              v-model="page"
+              :length="searchResultsTotalCount / 100"
+            ></v-pagination>
           </div>
-          <div v-if="debounceTimer" class="text-center text-grey py-6">
-            <v-progress-circular indeterminate color="blue-lighten-2"></v-progress-circular>
-          </div>
-          <div v-else-if="searchResults.length === 0" class="text-center text-grey py-6">
-            No journals found for "{{ search }}".
+        </div>
+        <div v-if="debounceTimer" class="text-center text-grey py-6">
+          <v-progress-circular indeterminate color="blue-lighten-2"></v-progress-circular>
+        </div>
+        <div v-else-if="searchResults.length === 0" class="text-center text-grey py-6">
+          No journals found for "{{ search }}".
           </div>
         </div>
       </v-card>
@@ -293,12 +293,54 @@
     </v-card>
   </v-dialog>
 
+  <!-- Pending Change Dialog -->
+  <v-dialog v-model="isPendingDialogOpen" width="520">
+    <v-card rounded="xl" class="pa-2">
+      <v-card-title class="d-flex justify-space-between align-start w-100 pl-6">
+        <div style="flex: 1; min-width: 0; margin-right: 16px;">
+          <div>
+            Change Pending
+          </div>
+        </div>
+        <v-btn icon variant="text" class="mr-n4 mt-n2" style="flex-shrink: 0;" @click="isPendingDialogOpen = false">
+          <v-icon color="grey-darken-2">mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text>
+        <div>
+          A submitted change is currently pending for this journal. It will be processed within a few hours.
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+
+    <!-- No ISSN Dialog -->
+    <v-dialog v-model="isNoIssnDialogOpen" width="520">
+    <v-card rounded="xl" class="pa-2">
+      <v-card-title class="d-flex justify-space-between align-start w-100 pl-6">
+        <div style="flex: 1; min-width: 0; margin-right: 16px;">
+          <div>
+            ISSN Missing
+          </div>
+        </div>
+        <v-btn icon variant="text" class="mr-n4 mt-n2" style="flex-shrink: 0;" @click="isNoIssnDialogOpen = false">
+          <v-icon color="grey-darken-2">mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text>
+        <div>
+          This journal record is missing an ISSN. We currently require an ISSN to submit corrections.
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+
 </template>
 
 
 <script setup>
 
-import { ref, computed, watch, onMounted } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useStore } from 'vuex';
 import axios from 'axios';
 
@@ -310,11 +352,12 @@ const store = useStore();
 const correctionsHost = "https://corrections.openalex.org";
 //const correctionsHost = "http://localhost:5006";
 
-const search = useParams('search', 'string', '');
-const searchResults = ref([]);
+const search                  = useParams('search', 'string', '');
+const searchResults           = ref([]);
 const searchResultsTotalCount = ref(0);
-const openAccessFilter = useParams('openAccessFilter', 'string', 'all');
-const worksFilter = useParams('worksFilter', 'number', 0);
+const openAccessFilter        = useParams('openAccessFilter', 'string', 'all');
+const worksFilter             = useParams('worksFilter', 'number', 0);
+const page                    = useParams('page', 'number', 1);
 
 const pendingCorrections = ref([]);
 
@@ -328,6 +371,8 @@ const alwaysOA = ref(true);
 const oaDate = ref(null);
 
 const isLibrarianDialogOpen = ref(false);
+const isPendingDialogOpen = ref(false);
+const isNoIssnDialogOpen = ref(false);
 const email = computed(() => store.getters['user/userEmail']);
 const isLibrarian = computed(() => store.getters['user/isLibrarian']);
 
@@ -339,11 +384,10 @@ let currentRequestId = 0;
 let errorTimer = null;
 
 const headers = [
-  { title: 'Title', key: 'display_name', width: '350px', align: 'start' },
-  { title: 'ISSN', key: 'issn_l', width: '150px', align: 'start', sortable: false },
-  { title: 'OA', key: 'is_oa' },
-  { title: 'Works', key: 'works_count', align: 'end' },
-  { title: '', key: 'homepage_url', align: 'end', sortable: false },
+  { title: '', key: 'is_oa', width: '10px' },
+  { title: 'Title', key: 'display_name', align: 'start' },
+  { title: 'Works', key: 'works_count', width: '100px', align: 'end' },
+  { title: '', key: 'homepage_url', width: '40px', align: 'end', sortable: false },
 ];
 
 const openAccessFilterString = computed(() => {
@@ -374,9 +418,24 @@ const editJournal = (journal) => {
     isLibrarianDialogOpen.value = true;
     return;
   }
+
+  if (!journal.issn_l) {
+    isNoIssnDialogOpen.value = true;
+    return;
+  }
+
+  if (pendingCorrections.value.includes(journal.issn_l)) {
+    isPendingDialogOpen.value = true;
+    return;
+  }
+
   editingJournal.value = journal;
   editDialog.value = true;
   correctedOA.value = journal.is_oa;
+};
+
+const onRowClick = (event, item) => {
+  editJournal(item.item);
 };
 
 const isYear = (value) => {
@@ -409,7 +468,7 @@ const getSearchResults = async () => {
     if (search.value) {
       filter += isISSN(search.value) ? `,issn_l:${search.value}` : `,display_name.search:${search.value}`;
     }
-    const response = await axios.get(`https://api.openalex.org/sources?filter=${filter}${openAccessFilterString.value}${worksFilterString.value}&per_page=100&sort=works_count:desc`);
+    const response = await axios.get(`https://api.openalex.org/sources?filter=${filter}${openAccessFilterString.value}${worksFilterString.value}&per_page=100&sort=works_count:desc&page=${page.value}`);
     
     // Only update results if this is still the most recent request
     if (requestId === currentRequestId) {
@@ -428,11 +487,6 @@ const getSearchResults = async () => {
 
 const isISSN = (issn) => {
   return /^\d{4}-\d{3}[0-9X]$/.test(issn);
-};
-
-const getDomain = (url) => {
-  const domain = url.replace('https://', '').replace('http://', '').replace('www.', '');
-  return domain.split('/')[0];
 };
 
 const debouncedSearch = () => {
@@ -478,20 +532,28 @@ function generatePostData() {
   return post;
 }
 
-onMounted(() => {
-  getPendingCorrections();
-});
+getPendingCorrections();
+debouncedSearch();
 
 watch(search, () => {
+  searchResults.value = [];
+  page.value = 1;
   debouncedSearch();
-}, { immediate: true });
+});
 
 watch(openAccessFilter, () => {
   searchResults.value = [];
+  page.value = 1;
   debouncedSearch();
 });
 
 watch(worksFilter, () => {
+  searchResults.value = [];
+  page.value = 1;
+  debouncedSearch();
+});
+
+watch(page, () => {
   searchResults.value = [];
   debouncedSearch();
 });
@@ -539,8 +601,9 @@ watch(showErrorMessage, () => {
   font-size: 12px;
   height: 36px !important;
 }
-.journal-title:hover {
-text-decoration: underline;
+:deep(.v-data-table tbody tr:hover) {
+  cursor: pointer;
+  background-color: #F5F5F5;
 }
 .ellipsis-2-lines {
   display: -webkit-box;
